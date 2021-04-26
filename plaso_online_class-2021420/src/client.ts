96f8e6633b0668f1bb0b93f012c8d253706db372
sdk-team <sdk-team@alibabacloud.com>
2021-04-26 01:31:03 +0000

    Generated ts 2021-4-20 for plaso_online_class.

--- plaso_online_class-2021420/src/client.ts
+++ plaso_online_class-2021420/src/client.ts
@@ -8,6 +8,108 @@ import OpenApiUtil from '@alicloud/openapi-util';
 import EndpointUtil from '@alicloud/endpoint-util';
 import * as $tea from '@alicloud/tea-typescript';
 
+export class DataCallbackRequest extends $tea.Model {
+  recordId?: string;
+  teacherId?: string;
+  signature?: string;
+  validBegin?: number;
+  appId?: string;
+  topic?: string;
+  meetingId?: string;
+  validTime?: number;
+  beginTime?: number;
+  endTime?: number;
+  cover?: string;
+  duration?: string;
+  listenerCount?: number;
+  userCount?: number;
+  static names(): { [key: string]: string } {
+    return {
+      recordId: 'RecordId',
+      teacherId: 'TeacherId',
+      signature: 'Signature',
+      validBegin: 'ValidBegin',
+      appId: 'AppId',
+      topic: 'Topic',
+      meetingId: 'MeetingId',
+      validTime: 'ValidTime',
+      beginTime: 'BeginTime',
+      endTime: 'EndTime',
+      cover: 'Cover',
+      duration: 'Duration',
+      listenerCount: 'ListenerCount',
+      userCount: 'UserCount',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      recordId: 'string',
+      teacherId: 'string',
+      signature: 'string',
+      validBegin: 'number',
+      appId: 'string',
+      topic: 'string',
+      meetingId: 'string',
+      validTime: 'number',
+      beginTime: 'number',
+      endTime: 'number',
+      cover: 'string',
+      duration: 'string',
+      listenerCount: 'number',
+      userCount: 'number',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class DataCallbackResponseBody extends $tea.Model {
+  msg?: string;
+  code?: number;
+  static names(): { [key: string]: string } {
+    return {
+      msg: 'Msg',
+      code: 'Code',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      msg: 'string',
+      code: 'number',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class DataCallbackResponse extends $tea.Model {
+  headers: { [key: string]: string };
+  body: DataCallbackResponseBody;
+  static names(): { [key: string]: string } {
+    return {
+      headers: 'headers',
+      body: 'body',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      headers: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+      body: DataCallbackResponseBody,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class EndCallbackRequest extends $tea.Model {
   opTime?: number;
   signature?: string;
@@ -171,6 +273,78 @@ export default class Client extends OpenApi {
     return EndpointUtil.getEndpointRules(productId, regionId, endpointRule, network, suffix);
   }
 
+  async dataCallback(request: DataCallbackRequest): Promise<DataCallbackResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.dataCallbackWithOptions(request, headers, runtime);
+  }
+
+  async dataCallbackWithOptions(request: DataCallbackRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<DataCallbackResponse> {
+    Util.validateModel(request);
+    let query : {[key: string ]: any} = { };
+    if (!Util.isUnset(request.recordId)) {
+      query["RecordId"] = request.recordId;
+    }
+
+    if (!Util.isUnset(request.teacherId)) {
+      query["TeacherId"] = request.teacherId;
+    }
+
+    if (!Util.isUnset(request.signature)) {
+      query["Signature"] = request.signature;
+    }
+
+    if (!Util.isUnset(request.validBegin)) {
+      query["ValidBegin"] = request.validBegin;
+    }
+
+    if (!Util.isUnset(request.appId)) {
+      query["AppId"] = request.appId;
+    }
+
+    if (!Util.isUnset(request.topic)) {
+      query["Topic"] = request.topic;
+    }
+
+    if (!Util.isUnset(request.meetingId)) {
+      query["MeetingId"] = request.meetingId;
+    }
+
+    if (!Util.isUnset(request.validTime)) {
+      query["ValidTime"] = request.validTime;
+    }
+
+    if (!Util.isUnset(request.beginTime)) {
+      query["BeginTime"] = request.beginTime;
+    }
+
+    if (!Util.isUnset(request.endTime)) {
+      query["EndTime"] = request.endTime;
+    }
+
+    if (!Util.isUnset(request.cover)) {
+      query["Cover"] = request.cover;
+    }
+
+    if (!Util.isUnset(request.duration)) {
+      query["Duration"] = request.duration;
+    }
+
+    if (!Util.isUnset(request.listenerCount)) {
+      query["ListenerCount"] = request.listenerCount;
+    }
+
+    if (!Util.isUnset(request.userCount)) {
+      query["UserCount"] = request.userCount;
+    }
+
+    let req = new $OpenApi.OpenApiRequest({
+      headers: headers,
+      query: OpenApiUtil.query(query),
+    });
+    return $tea.cast<DataCallbackResponse>(await this.doROARequest("DataCallback", "2021-4-20", "HTTPS", "POST", "AK", `/plaso/datacallback`, "json", req, runtime), new DataCallbackResponse({}));
+  }
+
   async endCallback(request: EndCallbackRequest): Promise<EndCallbackResponse> {
     let runtime = new $Util.RuntimeOptions({ });
     let headers : {[key: string ]: string} = { };
@@ -180,6 +354,10 @@ export default class Client extends OpenApi {
   async endCallbackWithOptions(request: EndCallbackRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<EndCallbackResponse> {
     Util.validateModel(request);
     let query : {[key: string ]: any} = { };
+    if (!Util.isUnset(request.opTime)) {
+      query["OpTime"] = request.opTime;
+    }
+
     if (!Util.isUnset(request.signature)) {
       query["Signature"] = request.signature;
     }
@@ -204,17 +382,11 @@ export default class Client extends OpenApi {
       query["NewEndTime"] = request.newEndTime;
     }
 
-    let body : {[key: string ]: any} = { };
-    if (!Util.isUnset(request.opTime)) {
-      body["OpTime"] = request.opTime;
-    }
-
     let req = new $OpenApi.OpenApiRequest({
       headers: headers,
       query: OpenApiUtil.query(query),
-      body: OpenApiUtil.parseToMap(body),
     });
-    return $tea.cast<EndCallbackResponse>(await this.doROARequestWithForm("EndCallback", "2021-4-20", "HTTPS", "POST", "AK", `/plaso/roa`, "json", req, runtime), new EndCallbackResponse({}));
+    return $tea.cast<EndCallbackResponse>(await this.doROARequest("EndCallback", "2021-4-20", "HTTPS", "POST", "AK", `/plaso/roa`, "json", req, runtime), new EndCallbackResponse({}));
   }
 
 }
