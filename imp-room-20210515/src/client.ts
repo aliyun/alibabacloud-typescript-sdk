427af726a5599896e42c6ca9ededc31cb4836c34
sdk-team <sdk-team@alibabacloud.com>
2021-06-01 11:20:00 +0000

    Add GetRoomDetail and GetRoomList.

--- imp-room-20210515/src/client.ts
+++ imp-room-20210515/src/client.ts
@@ -315,6 +315,150 @@ export class CreateInstanceResponse extends $tea.Model {
   }
 }
 
+export class GetRoomDetailRequest extends $tea.Model {
+  request?: GetRoomDetailRequestRequest;
+  static names(): { [key: string]: string } {
+    return {
+      request: 'Request',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      request: GetRoomDetailRequestRequest,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetRoomDetailResponseBody extends $tea.Model {
+  requestId?: string;
+  result?: GetRoomDetailResponseBodyResult;
+  responseSuccess?: boolean;
+  errorCode?: string;
+  errorMsg?: string;
+  static names(): { [key: string]: string } {
+    return {
+      requestId: 'RequestId',
+      result: 'Result',
+      responseSuccess: 'ResponseSuccess',
+      errorCode: 'ErrorCode',
+      errorMsg: 'ErrorMsg',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      requestId: 'string',
+      result: GetRoomDetailResponseBodyResult,
+      responseSuccess: 'boolean',
+      errorCode: 'string',
+      errorMsg: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetRoomDetailResponse extends $tea.Model {
+  headers: { [key: string]: string };
+  body: GetRoomDetailResponseBody;
+  static names(): { [key: string]: string } {
+    return {
+      headers: 'headers',
+      body: 'body',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      headers: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+      body: GetRoomDetailResponseBody,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetRoomListRequest extends $tea.Model {
+  request?: GetRoomListRequestRequest;
+  static names(): { [key: string]: string } {
+    return {
+      request: 'Request',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      request: GetRoomListRequestRequest,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetRoomListResponseBody extends $tea.Model {
+  requestId?: string;
+  result?: GetRoomListResponseBodyResult;
+  responseSuccess?: boolean;
+  errorCode?: string;
+  errorMsg?: string;
+  static names(): { [key: string]: string } {
+    return {
+      requestId: 'RequestId',
+      result: 'Result',
+      responseSuccess: 'ResponseSuccess',
+      errorCode: 'ErrorCode',
+      errorMsg: 'ErrorMsg',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      requestId: 'string',
+      result: GetRoomListResponseBodyResult,
+      responseSuccess: 'boolean',
+      errorCode: 'string',
+      errorMsg: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetRoomListResponse extends $tea.Model {
+  headers: { [key: string]: string };
+  body: GetRoomListResponseBody;
+  static names(): { [key: string]: string } {
+    return {
+      headers: 'headers',
+      body: 'body',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      headers: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+      body: GetRoomListResponseBody,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class GetLoginTokenRequestRequestParams extends $tea.Model {
   appUid?: string;
   appKey?: string;
@@ -496,6 +640,177 @@ export class CreateInstanceResponseBodyResult extends $tea.Model {
   }
 }
 
+export class GetRoomDetailRequestRequest extends $tea.Model {
+  domain?: string;
+  roomId?: string;
+  static names(): { [key: string]: string } {
+    return {
+      domain: 'Domain',
+      roomId: 'RoomId',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      domain: 'string',
+      roomId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetRoomDetailResponseBodyResultPluginInstanceInfoList extends $tea.Model {
+  pluginId?: string;
+  instanceId?: string;
+  createTime?: number;
+  extension?: { [key: string]: string };
+  static names(): { [key: string]: string } {
+    return {
+      pluginId: 'PluginId',
+      instanceId: 'InstanceId',
+      createTime: 'CreateTime',
+      extension: 'Extension',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      pluginId: 'string',
+      instanceId: 'string',
+      createTime: 'number',
+      extension: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetRoomDetailResponseBodyResult extends $tea.Model {
+  roomId?: string;
+  title?: string;
+  notice?: string;
+  ownerId?: string;
+  uv?: number;
+  onlineCount?: number;
+  pluginInstanceInfoList?: GetRoomDetailResponseBodyResultPluginInstanceInfoList[];
+  static names(): { [key: string]: string } {
+    return {
+      roomId: 'RoomId',
+      title: 'Title',
+      notice: 'Notice',
+      ownerId: 'OwnerId',
+      uv: 'Uv',
+      onlineCount: 'OnlineCount',
+      pluginInstanceInfoList: 'PluginInstanceInfoList',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      roomId: 'string',
+      title: 'string',
+      notice: 'string',
+      ownerId: 'string',
+      uv: 'number',
+      onlineCount: 'number',
+      pluginInstanceInfoList: { 'type': 'array', 'itemType': GetRoomDetailResponseBodyResultPluginInstanceInfoList },
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetRoomListRequestRequest extends $tea.Model {
+  domain?: string;
+  bizType?: string;
+  pageNum?: number;
+  pageSize?: number;
+  static names(): { [key: string]: string } {
+    return {
+      domain: 'Domain',
+      bizType: 'BizType',
+      pageNum: 'PageNum',
+      pageSize: 'PageSize',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      domain: 'string',
+      bizType: 'string',
+      pageNum: 'number',
+      pageSize: 'number',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetRoomListResponseBodyResultRoomInfoList extends $tea.Model {
+  roomId?: string;
+  title?: string;
+  ownerId?: string;
+  bizType?: string;
+  domain?: string;
+  static names(): { [key: string]: string } {
+    return {
+      roomId: 'RoomId',
+      title: 'Title',
+      ownerId: 'OwnerId',
+      bizType: 'BizType',
+      domain: 'Domain',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      roomId: 'string',
+      title: 'string',
+      ownerId: 'string',
+      bizType: 'string',
+      domain: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetRoomListResponseBodyResult extends $tea.Model {
+  total?: number;
+  hasMore?: boolean;
+  roomInfoList?: GetRoomListResponseBodyResultRoomInfoList[];
+  static names(): { [key: string]: string } {
+    return {
+      total: 'Total',
+      hasMore: 'HasMore',
+      roomInfoList: 'RoomInfoList',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      total: 'number',
+      hasMore: 'boolean',
+      roomInfoList: { 'type': 'array', 'itemType': GetRoomListResponseBodyResultRoomInfoList },
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 
 export default class Client extends OpenApi {
 
@@ -575,6 +890,32 @@ export default class Client extends OpenApi {
   async createInstance(request: CreateInstanceRequest): Promise<CreateInstanceResponse> {
     let runtime = new $Util.RuntimeOptions({ });
     return await this.createInstanceWithOptions(request, runtime);
+  }
+
+  async getRoomDetailWithOptions(request: GetRoomDetailRequest, runtime: $Util.RuntimeOptions): Promise<GetRoomDetailResponse> {
+    Util.validateModel(request);
+    let req = new $OpenApi.OpenApiRequest({
+      body: Util.toMap(request),
+    });
+    return $tea.cast<GetRoomDetailResponse>(await this.doRPCRequest("GetRoomDetail", "2021-05-15", "HTTPS", "POST", "AK", "json", req, runtime), new GetRoomDetailResponse({}));
+  }
+
+  async getRoomDetail(request: GetRoomDetailRequest): Promise<GetRoomDetailResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    return await this.getRoomDetailWithOptions(request, runtime);
+  }
+
+  async getRoomListWithOptions(request: GetRoomListRequest, runtime: $Util.RuntimeOptions): Promise<GetRoomListResponse> {
+    Util.validateModel(request);
+    let req = new $OpenApi.OpenApiRequest({
+      body: Util.toMap(request),
+    });
+    return $tea.cast<GetRoomListResponse>(await this.doRPCRequest("GetRoomList", "2021-05-15", "HTTPS", "POST", "AK", "json", req, runtime), new GetRoomListResponse({}));
+  }
+
+  async getRoomList(request: GetRoomListRequest): Promise<GetRoomListResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    return await this.getRoomListWithOptions(request, runtime);
   }
 
 }
