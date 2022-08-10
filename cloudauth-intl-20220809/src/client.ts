25f72040d88e8bd50c89a32ad0abf069459d5782
sdk-team <sdk-team@alibabacloud.com>
2022-08-10 01:44:27 +0000

    Support initialize for ekyc.

---
+++ cloudauth-intl-20220809/src/client.ts
@@ -0,0 +1,224 @@
+// This file is auto-generated, don't edit it
+/**
+ *
+ */
+import Util, * as $Util from '@alicloud/tea-util';
+import OpenApi, * as $OpenApi from '@alicloud/openapi-client';
+import OpenApiUtil from '@alicloud/openapi-util';
+import EndpointUtil from '@alicloud/endpoint-util';
+import * as $tea from '@alicloud/tea-typescript';
+
+export class InitializeRequest extends $tea.Model {
+  docType?: string;
+  flowType?: string;
+  merchantBizId?: string;
+  merchantUserId?: string;
+  metaInfo?: string;
+  operationMode?: string;
+  pages?: string;
+  productCode?: string;
+  productConfig?: string;
+  sceneCode?: string;
+  serviceLevel?: string;
+  static names(): { [key: string]: string } {
+    return {
+      docType: 'DocType',
+      flowType: 'FlowType',
+      merchantBizId: 'MerchantBizId',
+      merchantUserId: 'MerchantUserId',
+      metaInfo: 'MetaInfo',
+      operationMode: 'OperationMode',
+      pages: 'Pages',
+      productCode: 'ProductCode',
+      productConfig: 'ProductConfig',
+      sceneCode: 'SceneCode',
+      serviceLevel: 'ServiceLevel',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      docType: 'string',
+      flowType: 'string',
+      merchantBizId: 'string',
+      merchantUserId: 'string',
+      metaInfo: 'string',
+      operationMode: 'string',
+      pages: 'string',
+      productCode: 'string',
+      productConfig: 'string',
+      sceneCode: 'string',
+      serviceLevel: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class InitializeResponseBody extends $tea.Model {
+  code?: string;
+  message?: string;
+  requestId?: string;
+  result?: InitializeResponseBodyResult;
+  static names(): { [key: string]: string } {
+    return {
+      code: 'Code',
+      message: 'Message',
+      requestId: 'RequestId',
+      result: 'Result',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      code: 'string',
+      message: 'string',
+      requestId: 'string',
+      result: InitializeResponseBodyResult,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class InitializeResponse extends $tea.Model {
+  headers: { [key: string]: string };
+  statusCode: number;
+  body: InitializeResponseBody;
+  static names(): { [key: string]: string } {
+    return {
+      headers: 'headers',
+      statusCode: 'statusCode',
+      body: 'body',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      headers: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+      statusCode: 'number',
+      body: InitializeResponseBody,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class InitializeResponseBodyResult extends $tea.Model {
+  transactionId?: string;
+  static names(): { [key: string]: string } {
+    return {
+      transactionId: 'TransactionId',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      transactionId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+
+export default class Client extends OpenApi {
+
+  constructor(config: $OpenApi.Config) {
+    super(config);
+    this._endpointRule = "";
+    this.checkConfig(config);
+    this._endpoint = this.getEndpoint("cloudauth-intl", this._regionId, this._endpointRule, this._network, this._suffix, this._endpointMap, this._endpoint);
+  }
+
+
+  getEndpoint(productId: string, regionId: string, endpointRule: string, network: string, suffix: string, endpointMap: {[key: string ]: string}, endpoint: string): string {
+    if (!Util.empty(endpoint)) {
+      return endpoint;
+    }
+
+    if (!Util.isUnset(endpointMap) && !Util.empty(endpointMap[regionId])) {
+      return endpointMap[regionId];
+    }
+
+    return EndpointUtil.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+  }
+
+  async initializeWithOptions(request: InitializeRequest, runtime: $Util.RuntimeOptions): Promise<InitializeResponse> {
+    Util.validateModel(request);
+    let query = { };
+    if (!Util.isUnset(request.docType)) {
+      query["DocType"] = request.docType;
+    }
+
+    if (!Util.isUnset(request.flowType)) {
+      query["FlowType"] = request.flowType;
+    }
+
+    if (!Util.isUnset(request.merchantBizId)) {
+      query["MerchantBizId"] = request.merchantBizId;
+    }
+
+    if (!Util.isUnset(request.merchantUserId)) {
+      query["MerchantUserId"] = request.merchantUserId;
+    }
+
+    if (!Util.isUnset(request.metaInfo)) {
+      query["MetaInfo"] = request.metaInfo;
+    }
+
+    if (!Util.isUnset(request.operationMode)) {
+      query["OperationMode"] = request.operationMode;
+    }
+
+    if (!Util.isUnset(request.pages)) {
+      query["Pages"] = request.pages;
+    }
+
+    if (!Util.isUnset(request.productCode)) {
+      query["ProductCode"] = request.productCode;
+    }
+
+    if (!Util.isUnset(request.productConfig)) {
+      query["ProductConfig"] = request.productConfig;
+    }
+
+    if (!Util.isUnset(request.sceneCode)) {
+      query["SceneCode"] = request.sceneCode;
+    }
+
+    if (!Util.isUnset(request.serviceLevel)) {
+      query["ServiceLevel"] = request.serviceLevel;
+    }
+
+    let req = new $OpenApi.OpenApiRequest({
+      query: OpenApiUtil.query(query),
+    });
+    let params = new $OpenApi.Params({
+      action: "Initialize",
+      version: "2022-08-09",
+      protocol: "HTTPS",
+      pathname: "/",
+      method: "POST",
+      authType: "AK",
+      style: "RPC",
+      reqBodyType: "formData",
+      bodyType: "json",
+    });
+    return $tea.cast<InitializeResponse>(await this.callApi(params, req, runtime), new InitializeResponse({}));
+  }
+
+  async initialize(request: InitializeRequest): Promise<InitializeResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    return await this.initializeWithOptions(request, runtime);
+  }
+
+}
