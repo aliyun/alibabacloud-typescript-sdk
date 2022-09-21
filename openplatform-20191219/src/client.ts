64c30c948f62616c9f7775cc2063efe3c89d3ef9
sdk-team <sdk-team@alibabacloud.com>
2022-09-21 06:15:40 +0000

    Generated ts 2019-12-19 for OpenPlatform.

--- openplatform-20191219/src/client.ts
+++ openplatform-20191219/src/client.ts
@@ -1,11 +1,15 @@
 // This file is auto-generated, don't edit it
+/**
+ *
+ */
 import Util, * as $Util from '@alicloud/tea-util';
-import RPC, * as $RPC from '@alicloud/rpc-client';
+import OpenApi, * as $OpenApi from '@alicloud/openapi-client';
+import OpenApiUtil from '@alicloud/openapi-util';
 import EndpointUtil from '@alicloud/endpoint-util';
 import * as $tea from '@alicloud/tea-typescript';
 
 export class AuthorizeFileUploadRequest extends $tea.Model {
-  product: string;
+  product?: string;
   regionId?: string;
   static names(): { [key: string]: string } {
     return {
@@ -26,15 +30,15 @@ export class AuthorizeFileUploadRequest extends $tea.Model {
   }
 }
 
-export class AuthorizeFileUploadResponse extends $tea.Model {
-  accessKeyId: string;
-  bucket: string;
-  encodedPolicy: string;
-  endpoint: string;
-  objectKey: string;
-  requestId: string;
-  signature: string;
-  useAccelerate: boolean;
+export class AuthorizeFileUploadResponseBody extends $tea.Model {
+  accessKeyId?: string;
+  bucket?: string;
+  encodedPolicy?: string;
+  endpoint?: string;
+  objectKey?: string;
+  requestId?: string;
+  signature?: string;
+  useAccelerate?: boolean;
   static names(): { [key: string]: string } {
     return {
       accessKeyId: 'AccessKeyId',
@@ -66,26 +70,41 @@ export class AuthorizeFileUploadResponse extends $tea.Model {
   }
 }
 
+export class AuthorizeFileUploadResponse extends $tea.Model {
+  headers: { [key: string]: string };
+  statusCode: number;
+  body: AuthorizeFileUploadResponseBody;
+  static names(): { [key: string]: string } {
+    return {
+      headers: 'headers',
+      statusCode: 'statusCode',
+      body: 'body',
+    };
+  }
 
-export default class Client extends RPC {
+  static types(): { [key: string]: any } {
+    return {
+      headers: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+      statusCode: 'number',
+      body: AuthorizeFileUploadResponseBody,
+    };
+  }
 
-  constructor(config: $RPC.Config) {
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+
+export default class Client extends OpenApi {
+
+  constructor(config: $OpenApi.Config) {
     super(config);
     this._endpointRule = "";
     this.checkConfig(config);
     this._endpoint = this.getEndpoint("openplatform", this._regionId, this._endpointRule, this._network, this._suffix, this._endpointMap, this._endpoint);
   }
 
-
-  async authorizeFileUploadWithOptions(request: AuthorizeFileUploadRequest, runtime: $Util.RuntimeOptions): Promise<AuthorizeFileUploadResponse> {
-    Util.validateModel(request);
-    return $tea.cast<AuthorizeFileUploadResponse>(await this.doRequest("AuthorizeFileUpload", "HTTPS", "GET", "2019-12-19", "AK", $tea.toMap(request), null, runtime), new AuthorizeFileUploadResponse({}));
-  }
-
-  async authorizeFileUpload(request: AuthorizeFileUploadRequest): Promise<AuthorizeFileUploadResponse> {
-    let runtime = new $Util.RuntimeOptions({ });
-    return await this.authorizeFileUploadWithOptions(request, runtime);
-  }
 
   getEndpoint(productId: string, regionId: string, endpointRule: string, network: string, suffix: string, endpointMap: {[key: string ]: string}, endpoint: string): string {
     if (!Util.empty(endpoint)) {
@@ -97,6 +116,31 @@ export default class Client extends RPC {
     }
 
     return EndpointUtil.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+  }
+
+  async authorizeFileUploadWithOptions(request: AuthorizeFileUploadRequest, runtime: $Util.RuntimeOptions): Promise<AuthorizeFileUploadResponse> {
+    Util.validateModel(request);
+    let query = OpenApiUtil.query(Util.toMap(request));
+    let req = new $OpenApi.OpenApiRequest({
+      query: OpenApiUtil.query(query),
+    });
+    let params = new $OpenApi.Params({
+      action: "AuthorizeFileUpload",
+      version: "2019-12-19",
+      protocol: "HTTPS",
+      pathname: "/",
+      method: "GET",
+      authType: "AK",
+      style: "RPC",
+      reqBodyType: "formData",
+      bodyType: "json",
+    });
+    return $tea.cast<AuthorizeFileUploadResponse>(await this.callApi(params, req, runtime), new AuthorizeFileUploadResponse({}));
+  }
+
+  async authorizeFileUpload(request: AuthorizeFileUploadRequest): Promise<AuthorizeFileUploadResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    return await this.authorizeFileUploadWithOptions(request, runtime);
   }
 
 }
