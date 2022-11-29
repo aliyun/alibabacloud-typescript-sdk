9dd262df4c7aa8f087063dd32983aaf54ad93e0c
sdk-team <sdk-team@alibabacloud.com>
2022-11-29 09:36:21 +0000

    Support ent verify.

--- cloudauth-20221125/src/client.ts
+++ cloudauth-20221125/src/client.ts
@@ -2,10 +2,179 @@
 /**
  *
  */
-import Util from '@alicloud/tea-util';
+import Util, * as $Util from '@alicloud/tea-util';
 import OpenApi, * as $OpenApi from '@alicloud/openapi-client';
+import OpenApiUtil from '@alicloud/openapi-util';
 import EndpointUtil from '@alicloud/endpoint-util';
 import * as $tea from '@alicloud/tea-typescript';
+
+export class EntVerifyRequest extends $tea.Model {
+  accountNo?: string;
+  entName?: string;
+  infoVerifyType?: string;
+  legalPersonCertNo?: string;
+  legalPersonMobile?: string;
+  legalPersonName?: string;
+  licenseNo?: string;
+  merchantBizId?: string;
+  merchantUserId?: string;
+  riskModelVersion?: string;
+  riskVerifyType?: string;
+  sceneCode?: string;
+  userAuthorization?: string;
+  static names(): { [key: string]: string } {
+    return {
+      accountNo: 'AccountNo',
+      entName: 'EntName',
+      infoVerifyType: 'InfoVerifyType',
+      legalPersonCertNo: 'LegalPersonCertNo',
+      legalPersonMobile: 'LegalPersonMobile',
+      legalPersonName: 'LegalPersonName',
+      licenseNo: 'LicenseNo',
+      merchantBizId: 'MerchantBizId',
+      merchantUserId: 'MerchantUserId',
+      riskModelVersion: 'RiskModelVersion',
+      riskVerifyType: 'RiskVerifyType',
+      sceneCode: 'SceneCode',
+      userAuthorization: 'UserAuthorization',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      accountNo: 'string',
+      entName: 'string',
+      infoVerifyType: 'string',
+      legalPersonCertNo: 'string',
+      legalPersonMobile: 'string',
+      legalPersonName: 'string',
+      licenseNo: 'string',
+      merchantBizId: 'string',
+      merchantUserId: 'string',
+      riskModelVersion: 'string',
+      riskVerifyType: 'string',
+      sceneCode: 'string',
+      userAuthorization: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class EntVerifyResponseBody extends $tea.Model {
+  code?: string;
+  message?: string;
+  requestId?: string;
+  result?: EntVerifyResponseBodyResult;
+  static names(): { [key: string]: string } {
+    return {
+      code: 'Code',
+      message: 'Message',
+      requestId: 'RequestId',
+      result: 'Result',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      code: 'string',
+      message: 'string',
+      requestId: 'string',
+      result: EntVerifyResponseBodyResult,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class EntVerifyResponse extends $tea.Model {
+  headers: { [key: string]: string };
+  statusCode: number;
+  body: EntVerifyResponseBody;
+  static names(): { [key: string]: string } {
+    return {
+      headers: 'headers',
+      statusCode: 'statusCode',
+      body: 'body',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      headers: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+      statusCode: 'number',
+      body: EntVerifyResponseBody,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class EntVerifyResponseBodyResultRiskVerifyResultModelResults extends $tea.Model {
+  modelName?: string;
+  result?: string;
+  static names(): { [key: string]: string } {
+    return {
+      modelName: 'ModelName',
+      result: 'Result',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      modelName: 'string',
+      result: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class EntVerifyResponseBodyResultRiskVerifyResult extends $tea.Model {
+  modelResults?: EntVerifyResponseBodyResultRiskVerifyResultModelResults[];
+  static names(): { [key: string]: string } {
+    return {
+      modelResults: 'ModelResults',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      modelResults: { 'type': 'array', 'itemType': EntVerifyResponseBodyResultRiskVerifyResultModelResults },
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class EntVerifyResponseBodyResult extends $tea.Model {
+  riskVerifyResult?: EntVerifyResponseBodyResultRiskVerifyResult;
+  static names(): { [key: string]: string } {
+    return {
+      riskVerifyResult: 'RiskVerifyResult',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      riskVerifyResult: EntVerifyResponseBodyResultRiskVerifyResult,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
 
 
 export default class Client extends OpenApi {
@@ -28,6 +197,83 @@ export default class Client extends OpenApi {
     }
 
     return EndpointUtil.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+  }
+
+  async entVerifyWithOptions(request: EntVerifyRequest, runtime: $Util.RuntimeOptions): Promise<EntVerifyResponse> {
+    Util.validateModel(request);
+    let query = { };
+    if (!Util.isUnset(request.accountNo)) {
+      query["AccountNo"] = request.accountNo;
+    }
+
+    if (!Util.isUnset(request.entName)) {
+      query["EntName"] = request.entName;
+    }
+
+    if (!Util.isUnset(request.infoVerifyType)) {
+      query["InfoVerifyType"] = request.infoVerifyType;
+    }
+
+    if (!Util.isUnset(request.legalPersonCertNo)) {
+      query["LegalPersonCertNo"] = request.legalPersonCertNo;
+    }
+
+    if (!Util.isUnset(request.legalPersonMobile)) {
+      query["LegalPersonMobile"] = request.legalPersonMobile;
+    }
+
+    if (!Util.isUnset(request.legalPersonName)) {
+      query["LegalPersonName"] = request.legalPersonName;
+    }
+
+    if (!Util.isUnset(request.licenseNo)) {
+      query["LicenseNo"] = request.licenseNo;
+    }
+
+    if (!Util.isUnset(request.merchantBizId)) {
+      query["MerchantBizId"] = request.merchantBizId;
+    }
+
+    if (!Util.isUnset(request.merchantUserId)) {
+      query["MerchantUserId"] = request.merchantUserId;
+    }
+
+    if (!Util.isUnset(request.riskModelVersion)) {
+      query["RiskModelVersion"] = request.riskModelVersion;
+    }
+
+    if (!Util.isUnset(request.riskVerifyType)) {
+      query["RiskVerifyType"] = request.riskVerifyType;
+    }
+
+    if (!Util.isUnset(request.sceneCode)) {
+      query["SceneCode"] = request.sceneCode;
+    }
+
+    if (!Util.isUnset(request.userAuthorization)) {
+      query["UserAuthorization"] = request.userAuthorization;
+    }
+
+    let req = new $OpenApi.OpenApiRequest({
+      query: OpenApiUtil.query(query),
+    });
+    let params = new $OpenApi.Params({
+      action: "EntVerify",
+      version: "2022-11-25",
+      protocol: "HTTPS",
+      pathname: "/",
+      method: "POST",
+      authType: "AK",
+      style: "RPC",
+      reqBodyType: "formData",
+      bodyType: "json",
+    });
+    return $tea.cast<EntVerifyResponse>(await this.callApi(params, req, runtime), new EntVerifyResponse({}));
+  }
+
+  async entVerify(request: EntVerifyRequest): Promise<EntVerifyResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    return await this.entVerifyWithOptions(request, runtime);
   }
 
 }
