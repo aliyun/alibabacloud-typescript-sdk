fccc2ba99c939e488b08a8407daa1877ed02098c
sdk-team <sdk-team@alibabacloud.com>
2022-12-14 10:33:44 +0000

    AuthCode.

--- appstream-center-20210218/src/client.ts
+++ appstream-center-20210218/src/client.ts
@@ -2,10 +2,108 @@
 /**
  *
  */
-import Util from '@alicloud/tea-util';
+import Util, * as $Util from '@alicloud/tea-util';
 import OpenApi, * as $OpenApi from '@alicloud/openapi-client';
+import OpenApiUtil from '@alicloud/openapi-util';
 import EndpointUtil from '@alicloud/endpoint-util';
 import * as $tea from '@alicloud/tea-typescript';
+
+export class GetAuthCodeRequest extends $tea.Model {
+  endUserId?: string;
+  externalUserId?: string;
+  policy?: string;
+  static names(): { [key: string]: string } {
+    return {
+      endUserId: 'EndUserId',
+      externalUserId: 'ExternalUserId',
+      policy: 'Policy',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      endUserId: 'string',
+      externalUserId: 'string',
+      policy: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetAuthCodeResponseBody extends $tea.Model {
+  authModel?: GetAuthCodeResponseBodyAuthModel;
+  requestId?: string;
+  static names(): { [key: string]: string } {
+    return {
+      authModel: 'AuthModel',
+      requestId: 'RequestId',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authModel: GetAuthCodeResponseBodyAuthModel,
+      requestId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetAuthCodeResponse extends $tea.Model {
+  headers: { [key: string]: string };
+  statusCode: number;
+  body: GetAuthCodeResponseBody;
+  static names(): { [key: string]: string } {
+    return {
+      headers: 'headers',
+      statusCode: 'statusCode',
+      body: 'body',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      headers: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+      statusCode: 'number',
+      body: GetAuthCodeResponseBody,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetAuthCodeResponseBodyAuthModel extends $tea.Model {
+  authCode?: string;
+  endUserId?: string;
+  expireTime?: string;
+  static names(): { [key: string]: string } {
+    return {
+      authCode: 'AuthCode',
+      endUserId: 'EndUserId',
+      expireTime: 'ExpireTime',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authCode: 'string',
+      endUserId: 'string',
+      expireTime: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
 
 
 export default class Client extends OpenApi {
@@ -28,6 +126,43 @@ export default class Client extends OpenApi {
     }
 
     return EndpointUtil.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+  }
+
+  async getAuthCodeWithOptions(request: GetAuthCodeRequest, runtime: $Util.RuntimeOptions): Promise<GetAuthCodeResponse> {
+    Util.validateModel(request);
+    let body : {[key: string ]: any} = { };
+    if (!Util.isUnset(request.endUserId)) {
+      body["EndUserId"] = request.endUserId;
+    }
+
+    if (!Util.isUnset(request.externalUserId)) {
+      body["ExternalUserId"] = request.externalUserId;
+    }
+
+    if (!Util.isUnset(request.policy)) {
+      body["Policy"] = request.policy;
+    }
+
+    let req = new $OpenApi.OpenApiRequest({
+      body: OpenApiUtil.parseToMap(body),
+    });
+    let params = new $OpenApi.Params({
+      action: "GetAuthCode",
+      version: "2021-02-18",
+      protocol: "HTTPS",
+      pathname: "/",
+      method: "POST",
+      authType: "AK",
+      style: "RPC",
+      reqBodyType: "formData",
+      bodyType: "json",
+    });
+    return $tea.cast<GetAuthCodeResponse>(await this.callApi(params, req, runtime), new GetAuthCodeResponse({}));
+  }
+
+  async getAuthCode(request: GetAuthCodeRequest): Promise<GetAuthCodeResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    return await this.getAuthCodeWithOptions(request, runtime);
   }
 
 }
