a74b2c01843e1bfe7353ae7ac8db048d35351af4
sdk-team <sdk-team@alibabacloud.com>
2023-07-24 15:11:10 +0000

    Add error code.

--- rds-20140815/src/client.ts
+++ rds-20140815/src/client.ts
@@ -42830,6 +42830,21 @@ export default class Client extends OpenApi {
     return await this.describeHistoryTasksWithOptions(request, runtime);
   }
 
+  /**
+    * > This operation supports only for ApsaraDB RDS for SQL Server instances and is available only to specific customers. If you want to call this operation, contact **Alibaba Cloud technical support**.
+    * ### Prerequisites
+    * The instance meets the following requirements:
+    * *   The instance resides in a region other than the China (Zhangjiakou) region.
+    * *   The instance runs RDS Basic Edition, runs SQL Server 2012 or later on RDS High-availability Edition, or runs RDS Cluster Edition.
+    * *   The instance belongs to the general-purpose or dedicated instance family. The shared instance family is not supported.
+    * *   The instance resides in a virtual private cloud (VPC). For more information about how to change the network type of an instance, see [Change the network type of an ApsaraDB RDS for SQL Server instance](~~95707~~).
+    * *   If the instance runs RDS High-availability Edition or RDS Cluster Edition, make sure that the instance is created on or after January 01, 2021. If the instance runs RDS Basic Edition, make sure that the instance is created on or after September 02, 2022. You can view the **Creation Time** parameter of an instance in the **Status** section of the **Basic Information** page in the ApsaraDB RDS console.
+    * Your **Alibaba Cloud account** is used for logons.
+    *
+    * @param request DescribeHostWebShellRequest
+    * @param runtime runtime options for this request RuntimeOptions
+    * @return DescribeHostWebShellResponse
+   */
   async describeHostWebShellWithOptions(request: DescribeHostWebShellRequest, runtime: $Util.RuntimeOptions): Promise<DescribeHostWebShellResponse> {
     Util.validateModel(request);
     let query = { };
@@ -42882,6 +42897,20 @@ export default class Client extends OpenApi {
     return $tea.cast<DescribeHostWebShellResponse>(await this.callApi(params, req, runtime), new DescribeHostWebShellResponse({}));
   }
 
+  /**
+    * > This operation supports only for ApsaraDB RDS for SQL Server instances and is available only to specific customers. If you want to call this operation, contact **Alibaba Cloud technical support**.
+    * ### Prerequisites
+    * The instance meets the following requirements:
+    * *   The instance resides in a region other than the China (Zhangjiakou) region.
+    * *   The instance runs RDS Basic Edition, runs SQL Server 2012 or later on RDS High-availability Edition, or runs RDS Cluster Edition.
+    * *   The instance belongs to the general-purpose or dedicated instance family. The shared instance family is not supported.
+    * *   The instance resides in a virtual private cloud (VPC). For more information about how to change the network type of an instance, see [Change the network type of an ApsaraDB RDS for SQL Server instance](~~95707~~).
+    * *   If the instance runs RDS High-availability Edition or RDS Cluster Edition, make sure that the instance is created on or after January 01, 2021. If the instance runs RDS Basic Edition, make sure that the instance is created on or after September 02, 2022. You can view the **Creation Time** parameter of an instance in the **Status** section of the **Basic Information** page in the ApsaraDB RDS console.
+    * Your **Alibaba Cloud account** is used for logons.
+    *
+    * @param request DescribeHostWebShellRequest
+    * @return DescribeHostWebShellResponse
+   */
   async describeHostWebShell(request: DescribeHostWebShellRequest): Promise<DescribeHostWebShellResponse> {
     let runtime = new $Util.RuntimeOptions({ });
     return await this.describeHostWebShellWithOptions(request, runtime);
