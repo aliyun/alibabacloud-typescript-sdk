58812171b077f5839f4f112bf0d2459ce9be35ab
sdk-team <sdk-team@alibabacloud.com>
2023-08-23 10:02:55 +0000

    Generated ts 2022-11-25 for Cloudauth.

--- cloudauth-20221125/src/client.ts
+++ cloudauth-20221125/src/client.ts
@@ -104,6 +104,93 @@ export class EntElementVerifyResponse extends $tea.Model {
   }
 }
 
+export class EntRiskQueryRequest extends $tea.Model {
+  merchantBizId?: string;
+  merchantUserId?: string;
+  paramType?: string;
+  paramValue?: string;
+  sceneCode?: string;
+  userAuthorization?: string;
+  static names(): { [key: string]: string } {
+    return {
+      merchantBizId: 'MerchantBizId',
+      merchantUserId: 'MerchantUserId',
+      paramType: 'ParamType',
+      paramValue: 'ParamValue',
+      sceneCode: 'SceneCode',
+      userAuthorization: 'UserAuthorization',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      merchantBizId: 'string',
+      merchantUserId: 'string',
+      paramType: 'string',
+      paramValue: 'string',
+      sceneCode: 'string',
+      userAuthorization: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class EntRiskQueryResponseBody extends $tea.Model {
+  code?: string;
+  message?: string;
+  requestId?: string;
+  result?: EntRiskQueryResponseBodyResult;
+  static names(): { [key: string]: string } {
+    return {
+      code: 'Code',
+      message: 'Message',
+      requestId: 'RequestId',
+      result: 'Result',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      code: 'string',
+      message: 'string',
+      requestId: 'string',
+      result: EntRiskQueryResponseBodyResult,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class EntRiskQueryResponse extends $tea.Model {
+  headers: { [key: string]: string };
+  statusCode: number;
+  body: EntRiskQueryResponseBody;
+  static names(): { [key: string]: string } {
+    return {
+      headers: 'headers',
+      statusCode: 'statusCode',
+      body: 'body',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      headers: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+      statusCode: 'number',
+      body: EntRiskQueryResponseBody,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class EntVerifyRequest extends $tea.Model {
   accountNo?: string;
   entName?: string;
@@ -234,6 +321,74 @@ export class EntElementVerifyResponseBodyResult extends $tea.Model {
       openTime: 'string',
       reasonCode: 'string',
       reasonDetail: 'string',
+      status: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class EntRiskQueryResponseBodyResultRiskList extends $tea.Model {
+  creditCode?: string;
+  entName?: string;
+  listedDate?: string;
+  listedReason?: string;
+  operationOrg?: string;
+  regNo?: string;
+  removedDate?: string;
+  removedOrg?: string;
+  removedReason?: string;
+  static names(): { [key: string]: string } {
+    return {
+      creditCode: 'CreditCode',
+      entName: 'EntName',
+      listedDate: 'ListedDate',
+      listedReason: 'ListedReason',
+      operationOrg: 'OperationOrg',
+      regNo: 'RegNo',
+      removedDate: 'RemovedDate',
+      removedOrg: 'RemovedOrg',
+      removedReason: 'RemovedReason',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      creditCode: 'string',
+      entName: 'string',
+      listedDate: 'string',
+      listedReason: 'string',
+      operationOrg: 'string',
+      regNo: 'string',
+      removedDate: 'string',
+      removedOrg: 'string',
+      removedReason: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class EntRiskQueryResponseBodyResult extends $tea.Model {
+  bizCode?: string;
+  riskList?: EntRiskQueryResponseBodyResultRiskList[];
+  status?: string;
+  static names(): { [key: string]: string } {
+    return {
+      bizCode: 'BizCode',
+      riskList: 'RiskList',
+      status: 'Status',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      bizCode: 'string',
+      riskList: { 'type': 'array', 'itemType': EntRiskQueryResponseBodyResultRiskList },
       status: 'string',
     };
   }
@@ -388,6 +543,55 @@ export default class Client extends OpenApi {
   async entElementVerify(request: EntElementVerifyRequest): Promise<EntElementVerifyResponse> {
     let runtime = new $Util.RuntimeOptions({ });
     return await this.entElementVerifyWithOptions(request, runtime);
+  }
+
+  async entRiskQueryWithOptions(request: EntRiskQueryRequest, runtime: $Util.RuntimeOptions): Promise<EntRiskQueryResponse> {
+    Util.validateModel(request);
+    let query = { };
+    if (!Util.isUnset(request.merchantBizId)) {
+      query["MerchantBizId"] = request.merchantBizId;
+    }
+
+    if (!Util.isUnset(request.merchantUserId)) {
+      query["MerchantUserId"] = request.merchantUserId;
+    }
+
+    if (!Util.isUnset(request.paramType)) {
+      query["ParamType"] = request.paramType;
+    }
+
+    if (!Util.isUnset(request.paramValue)) {
+      query["ParamValue"] = request.paramValue;
+    }
+
+    if (!Util.isUnset(request.sceneCode)) {
+      query["SceneCode"] = request.sceneCode;
+    }
+
+    if (!Util.isUnset(request.userAuthorization)) {
+      query["UserAuthorization"] = request.userAuthorization;
+    }
+
+    let req = new $OpenApi.OpenApiRequest({
+      query: OpenApiUtil.query(query),
+    });
+    let params = new $OpenApi.Params({
+      action: "EntRiskQuery",
+      version: "2022-11-25",
+      protocol: "HTTPS",
+      pathname: "/",
+      method: "POST",
+      authType: "AK",
+      style: "RPC",
+      reqBodyType: "formData",
+      bodyType: "json",
+    });
+    return $tea.cast<EntRiskQueryResponse>(await this.callApi(params, req, runtime), new EntRiskQueryResponse({}));
+  }
+
+  async entRiskQuery(request: EntRiskQueryRequest): Promise<EntRiskQueryResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    return await this.entRiskQueryWithOptions(request, runtime);
   }
 
   async entVerifyWithOptions(request: EntVerifyRequest, runtime: $Util.RuntimeOptions): Promise<EntVerifyResponse> {
