87324ab76ea6eb064af9e638a68bddd734a65639
sdk-team <sdk-team@alibabacloud.com>
2023-12-20 17:09:12 +0000

    Generated ts 2016-04-28 for Vpc.

--- vpc-20160428/src/client.ts
+++ vpc-20160428/src/client.ts
@@ -24011,6 +24011,8 @@ export class ListFullNatEntriesRequest extends $tea.Model {
   ipProtocol?: string;
   maxResults?: number;
   natGatewayId?: string;
+  natIp?: string;
+  natIpPort?: string;
   networkInterfaceIds?: string[];
   nextToken?: string;
   ownerAccount?: string;
@@ -24027,6 +24029,8 @@ export class ListFullNatEntriesRequest extends $tea.Model {
       ipProtocol: 'IpProtocol',
       maxResults: 'MaxResults',
       natGatewayId: 'NatGatewayId',
+      natIp: 'NatIp',
+      natIpPort: 'NatIpPort',
       networkInterfaceIds: 'NetworkInterfaceIds',
       nextToken: 'NextToken',
       ownerAccount: 'OwnerAccount',
@@ -24046,6 +24050,8 @@ export class ListFullNatEntriesRequest extends $tea.Model {
       ipProtocol: 'string',
       maxResults: 'number',
       natGatewayId: 'string',
+      natIp: 'string',
+      natIpPort: 'string',
       networkInterfaceIds: { 'type': 'array', 'itemType': 'string' },
       nextToken: 'string',
       ownerAccount: 'string',
@@ -58979,13 +58985,13 @@ export default class Client extends OpenApi {
   }
 
   /**
-    * When you call this operation, take note of the following rules:
+    * When you call this operation, take note of the following items:
     * *   You can delete only routes that are in the **Available** state.
-    * *   You cannot delete a route of a virtual private cloud (VPC) in which a vSwitch or a route is being created.
+    * *   You cannot delete a route entry of a virtual private cloud (VPC) in which a vSwitch or another route entry is being created or deleted.
     * *   Before you call this operation to delete a route of a VBR route table, call the [DescribeRouteEntryList](~~138148~~) operation to query the **NextHopId** of the route first.
-    * *   The **DeleteRouteEntry** operation is asynchronous. After you send the request, the system returns a request ID. However, the operation is still being performed in the system background. You can call the [DescribeRouteEntryList](~~138148~~) operation to query the status of a route:
+    * *   **DeleteRouteEntry** is an asynchronous operation. After a request is sent, the system returns a request ID and runs the task in the background. You can call the [DescribeRouteEntryList](~~138148~~) operation to query the status of the task.
     *     *   If the route is in the **Deleting** state, the route is being deleted.
-    *     *   If you cannot query the route, the route is deleted.
+    *     *   If you cannot query the route entry, the route entry is deleted.
     * *   You cannot repeatedly call the **DeleteRouteEntry** operation to delete a route from the route table of a vRouter or a VBR within the specified period of time.
     *
     * @param request DeleteRouteEntryRequest
@@ -59053,13 +59059,13 @@ export default class Client extends OpenApi {
   }
 
   /**
-    * When you call this operation, take note of the following rules:
+    * When you call this operation, take note of the following items:
     * *   You can delete only routes that are in the **Available** state.
-    * *   You cannot delete a route of a virtual private cloud (VPC) in which a vSwitch or a route is being created.
+    * *   You cannot delete a route entry of a virtual private cloud (VPC) in which a vSwitch or another route entry is being created or deleted.
     * *   Before you call this operation to delete a route of a VBR route table, call the [DescribeRouteEntryList](~~138148~~) operation to query the **NextHopId** of the route first.
-    * *   The **DeleteRouteEntry** operation is asynchronous. After you send the request, the system returns a request ID. However, the operation is still being performed in the system background. You can call the [DescribeRouteEntryList](~~138148~~) operation to query the status of a route:
+    * *   **DeleteRouteEntry** is an asynchronous operation. After a request is sent, the system returns a request ID and runs the task in the background. You can call the [DescribeRouteEntryList](~~138148~~) operation to query the status of the task.
     *     *   If the route is in the **Deleting** state, the route is being deleted.
-    *     *   If you cannot query the route, the route is deleted.
+    *     *   If you cannot query the route entry, the route entry is deleted.
     * *   You cannot repeatedly call the **DeleteRouteEntry** operation to delete a route from the route table of a vRouter or a VBR within the specified period of time.
     *
     * @param request DeleteRouteEntryRequest
@@ -67387,6 +67393,14 @@ export default class Client extends OpenApi {
 
     if (!Util.isUnset(request.natGatewayId)) {
       query["NatGatewayId"] = request.natGatewayId;
+    }
+
+    if (!Util.isUnset(request.natIp)) {
+      query["NatIp"] = request.natIp;
+    }
+
+    if (!Util.isUnset(request.natIpPort)) {
+      query["NatIpPort"] = request.natIpPort;
     }
 
     if (!Util.isUnset(request.networkInterfaceIds)) {
