36db5098b3ad93ab7b59f390900f5d6888d6907e
sdk-team <sdk-team@alibabacloud.com>
2024-04-12 17:18:24 +0000

    Update API DescribeDedicatedHosts: add param MaxResults.

--- ecs-20140526/src/client.ts
+++ ecs-20140526/src/client.ts
@@ -12113,6 +12113,8 @@ export class DescribeDedicatedHostsRequest extends $tea.Model {
   dedicatedHostName?: string;
   dedicatedHostType?: string;
   lockReason?: string;
+  maxResults?: number;
+  nextToken?: string;
   ownerAccount?: string;
   ownerId?: number;
   pageNumber?: number;
@@ -12132,6 +12134,8 @@ export class DescribeDedicatedHostsRequest extends $tea.Model {
       dedicatedHostName: 'DedicatedHostName',
       dedicatedHostType: 'DedicatedHostType',
       lockReason: 'LockReason',
+      maxResults: 'MaxResults',
+      nextToken: 'NextToken',
       ownerAccount: 'OwnerAccount',
       ownerId: 'OwnerId',
       pageNumber: 'PageNumber',
@@ -12154,6 +12158,8 @@ export class DescribeDedicatedHostsRequest extends $tea.Model {
       dedicatedHostName: 'string',
       dedicatedHostType: 'string',
       lockReason: 'string',
+      maxResults: 'number',
+      nextToken: 'string',
       ownerAccount: 'string',
       ownerId: 'number',
       pageNumber: 'number',
@@ -12176,6 +12182,7 @@ export class DescribeDedicatedHostsRequest extends $tea.Model {
 
 export class DescribeDedicatedHostsResponseBody extends $tea.Model {
   dedicatedHosts?: DescribeDedicatedHostsResponseBodyDedicatedHosts;
+  nextToken?: string;
   pageNumber?: number;
   pageSize?: number;
   requestId?: string;
@@ -12183,6 +12190,7 @@ export class DescribeDedicatedHostsResponseBody extends $tea.Model {
   static names(): { [key: string]: string } {
     return {
       dedicatedHosts: 'DedicatedHosts',
+      nextToken: 'NextToken',
       pageNumber: 'PageNumber',
       pageSize: 'PageSize',
       requestId: 'RequestId',
@@ -12193,6 +12201,7 @@ export class DescribeDedicatedHostsResponseBody extends $tea.Model {
   static types(): { [key: string]: any } {
     return {
       dedicatedHosts: DescribeDedicatedHostsResponseBodyDedicatedHosts,
+      nextToken: 'string',
       pageNumber: 'number',
       pageSize: 'number',
       requestId: 'string',
@@ -15976,11 +15985,16 @@ export class DescribeInstanceTypeFamiliesResponse extends $tea.Model {
 
 export class DescribeInstanceTypesRequest extends $tea.Model {
   cpuArchitecture?: string;
+  cpuArchitectures?: string[];
   GPUSpec?: string;
+  gpuSpecs?: string[];
+  instanceCategories?: string[];
   instanceCategory?: string;
   instanceFamilyLevel?: string;
+  instanceTypeFamilies?: string[];
   instanceTypeFamily?: string;
   instanceTypes?: string[];
+  localStorageCategories?: string[];
   localStorageCategory?: string;
   maxResults?: number;
   maximumCpuCoreCount?: number;
@@ -16014,16 +16028,22 @@ export class DescribeInstanceTypesRequest extends $tea.Model {
   ownerAccount?: string;
   ownerId?: number;
   physicalProcessorModel?: string;
+  physicalProcessorModels?: string[];
   resourceOwnerAccount?: string;
   resourceOwnerId?: number;
   static names(): { [key: string]: string } {
     return {
       cpuArchitecture: 'CpuArchitecture',
+      cpuArchitectures: 'CpuArchitectures',
       GPUSpec: 'GPUSpec',
+      gpuSpecs: 'GpuSpecs',
+      instanceCategories: 'InstanceCategories',
       instanceCategory: 'InstanceCategory',
       instanceFamilyLevel: 'InstanceFamilyLevel',
+      instanceTypeFamilies: 'InstanceTypeFamilies',
       instanceTypeFamily: 'InstanceTypeFamily',
       instanceTypes: 'InstanceTypes',
+      localStorageCategories: 'LocalStorageCategories',
       localStorageCategory: 'LocalStorageCategory',
       maxResults: 'MaxResults',
       maximumCpuCoreCount: 'MaximumCpuCoreCount',
@@ -16057,6 +16077,7 @@ export class DescribeInstanceTypesRequest extends $tea.Model {
       ownerAccount: 'OwnerAccount',
       ownerId: 'OwnerId',
       physicalProcessorModel: 'PhysicalProcessorModel',
+      physicalProcessorModels: 'PhysicalProcessorModels',
       resourceOwnerAccount: 'ResourceOwnerAccount',
       resourceOwnerId: 'ResourceOwnerId',
     };
@@ -16065,11 +16086,16 @@ export class DescribeInstanceTypesRequest extends $tea.Model {
   static types(): { [key: string]: any } {
     return {
       cpuArchitecture: 'string',
+      cpuArchitectures: { 'type': 'array', 'itemType': 'string' },
       GPUSpec: 'string',
+      gpuSpecs: { 'type': 'array', 'itemType': 'string' },
+      instanceCategories: { 'type': 'array', 'itemType': 'string' },
       instanceCategory: 'string',
       instanceFamilyLevel: 'string',
+      instanceTypeFamilies: { 'type': 'array', 'itemType': 'string' },
       instanceTypeFamily: 'string',
       instanceTypes: { 'type': 'array', 'itemType': 'string' },
+      localStorageCategories: { 'type': 'array', 'itemType': 'string' },
       localStorageCategory: 'string',
       maxResults: 'number',
       maximumCpuCoreCount: 'number',
@@ -16103,6 +16129,7 @@ export class DescribeInstanceTypesRequest extends $tea.Model {
       ownerAccount: 'string',
       ownerId: 'number',
       physicalProcessorModel: 'string',
+      physicalProcessorModels: { 'type': 'array', 'itemType': 'string' },
       resourceOwnerAccount: 'string',
       resourceOwnerId: 'number',
     };
@@ -66059,6 +66086,14 @@ export default class Client extends OpenApi {
       query["LockReason"] = request.lockReason;
     }
 
+    if (!Util.isUnset(request.maxResults)) {
+      query["MaxResults"] = request.maxResults;
+    }
+
+    if (!Util.isUnset(request.nextToken)) {
+      query["NextToken"] = request.nextToken;
+    }
+
     if (!Util.isUnset(request.ownerAccount)) {
       query["OwnerAccount"] = request.ownerAccount;
     }
@@ -69015,8 +69050,20 @@ export default class Client extends OpenApi {
       query["CpuArchitecture"] = request.cpuArchitecture;
     }
 
+    if (!Util.isUnset(request.cpuArchitectures)) {
+      query["CpuArchitectures"] = request.cpuArchitectures;
+    }
+
     if (!Util.isUnset(request.GPUSpec)) {
       query["GPUSpec"] = request.GPUSpec;
+    }
+
+    if (!Util.isUnset(request.gpuSpecs)) {
+      query["GpuSpecs"] = request.gpuSpecs;
+    }
+
+    if (!Util.isUnset(request.instanceCategories)) {
+      query["InstanceCategories"] = request.instanceCategories;
     }
 
     if (!Util.isUnset(request.instanceCategory)) {
@@ -69027,12 +69074,20 @@ export default class Client extends OpenApi {
       query["InstanceFamilyLevel"] = request.instanceFamilyLevel;
     }
 
+    if (!Util.isUnset(request.instanceTypeFamilies)) {
+      query["InstanceTypeFamilies"] = request.instanceTypeFamilies;
+    }
+
     if (!Util.isUnset(request.instanceTypeFamily)) {
       query["InstanceTypeFamily"] = request.instanceTypeFamily;
     }
 
     if (!Util.isUnset(request.instanceTypes)) {
       query["InstanceTypes"] = request.instanceTypes;
+    }
+
+    if (!Util.isUnset(request.localStorageCategories)) {
+      query["LocalStorageCategories"] = request.localStorageCategories;
     }
 
     if (!Util.isUnset(request.localStorageCategory)) {
@@ -69165,6 +69220,10 @@ export default class Client extends OpenApi {
 
     if (!Util.isUnset(request.physicalProcessorModel)) {
       query["PhysicalProcessorModel"] = request.physicalProcessorModel;
+    }
+
+    if (!Util.isUnset(request.physicalProcessorModels)) {
+      query["PhysicalProcessorModels"] = request.physicalProcessorModels;
     }
 
     if (!Util.isUnset(request.resourceOwnerAccount)) {
@@ -69729,10 +69788,12 @@ export default class Client extends OpenApi {
   }
 
   /**
-    * - After you run a command, the command may not succeed or return the expected result. You can call this operation to query the execution result.
-    * - You can query information about command executions within the last four weeks. A maximum of 100,000 entries of execution information can be retained.
-    *   - Method 1: During a paged query, when you call the DescribeInvocations operation to retrieve the first page of results, set MaxResults to specify the maximum number of entries to return in the call. The return value of NextToken is a pagination token, which can be used in the next request to retrieve a new page of results. When you call the DescribeInvocations operation to retrieve a new page of results, set NextToken to the NextToken value returned in the previous call and set MaxResults to specify the maximum number of entries to return in this call. 
-    *   - Method 2: Use PageSize to specify the number of entries to return on each page, and then use PageNumber to specify the number of the page to return. You can use only one of the preceding methods. If you specify MaxResults or NextToken, the PageSize and PageNumber request parameters do not take effect and the TotalCount response parameter is invalid.
+    * ## [](#)Usage notes
+    * *   After you run a command, the command may not succeed or return the expected results. You can call this operation to query the execution results.
+    * *   You can query information about command executions within the last four weeks. Up to 100,000 pieces of execution information can be retained.
+    * *   You can use one of the following methods to check the responses:
+    *     *   Method 1: During a paged query, when you call the DescribeInvocations operation to retrieve the first page of results, use `MaxResults` to specify the maximum number of entries to return in the call. The return value of `NextToken` is a pagination token, which you can use in the next request to retrieve a new page of results. When you call the DescribeInvocations operation to retrieve a new page of results, set `NextToken` to the `NextToken` value returned in the previous call and set `MaxResults` to specify the maximum number of entries to return in this call.
+    *     *   Method 2: Use `PageSize` to specify the number of entries to return on each page, and then use `PageNumber` to specify the number of the page to return. You can use only one of the preceding methods. If you specify `MaxResults` or `NextToken`, the `PageSize` and `PageNumber` request parameters do not take effect and the `TotalCount` response parameter is invalid.
     *
     * @param request DescribeInvocationsRequest
     * @param runtime runtime options for this request RuntimeOptions
@@ -69847,10 +69908,12 @@ export default class Client extends OpenApi {
   }
 
   /**
-    * - After you run a command, the command may not succeed or return the expected result. You can call this operation to query the execution result.
-    * - You can query information about command executions within the last four weeks. A maximum of 100,000 entries of execution information can be retained.
-    *   - Method 1: During a paged query, when you call the DescribeInvocations operation to retrieve the first page of results, set MaxResults to specify the maximum number of entries to return in the call. The return value of NextToken is a pagination token, which can be used in the next request to retrieve a new page of results. When you call the DescribeInvocations operation to retrieve a new page of results, set NextToken to the NextToken value returned in the previous call and set MaxResults to specify the maximum number of entries to return in this call. 
-    *   - Method 2: Use PageSize to specify the number of entries to return on each page, and then use PageNumber to specify the number of the page to return. You can use only one of the preceding methods. If you specify MaxResults or NextToken, the PageSize and PageNumber request parameters do not take effect and the TotalCount response parameter is invalid.
+    * ## [](#)Usage notes
+    * *   After you run a command, the command may not succeed or return the expected results. You can call this operation to query the execution results.
+    * *   You can query information about command executions within the last four weeks. Up to 100,000 pieces of execution information can be retained.
+    * *   You can use one of the following methods to check the responses:
+    *     *   Method 1: During a paged query, when you call the DescribeInvocations operation to retrieve the first page of results, use `MaxResults` to specify the maximum number of entries to return in the call. The return value of `NextToken` is a pagination token, which you can use in the next request to retrieve a new page of results. When you call the DescribeInvocations operation to retrieve a new page of results, set `NextToken` to the `NextToken` value returned in the previous call and set `MaxResults` to specify the maximum number of entries to return in this call.
+    *     *   Method 2: Use `PageSize` to specify the number of entries to return on each page, and then use `PageNumber` to specify the number of the page to return. You can use only one of the preceding methods. If you specify `MaxResults` or `NextToken`, the `PageSize` and `PageNumber` request parameters do not take effect and the `TotalCount` response parameter is invalid.
     *
     * @param request DescribeInvocationsRequest
     * @return DescribeInvocationsResponse
@@ -80745,12 +80808,13 @@ export default class Client extends OpenApi {
   }
 
   /**
-    * In the security group-related API documents, inbound traffic refers to the traffic that is sent by the source device and received at the destination device.
-    * When you modify the rules of a security group by specifying the rule IDs, take note of the following limits:
-    * *   A security group authorization object can be one of the following types: IP address or CIDR block, security group, or prefix list. The type of an existing security group authorization object cannot be changed. If the original authorization object is an IP address, you can change it to another IP address or a CIDR block, but not to a security group or prefix list.
-    * *   The IP address family of the authorization object cannot be changed. For example, if the original authorization object is an IPv4 CIDR block, you cannot change it to an IPv6 CIDR block. If the original authorization object is a prefix list of an IPv4 address family, you cannot change it to a prefix list of an IPv6 address family.
-    * *   The modified security group rule cannot be the same as other existing rules.
-    * *   You cannot delete the value of a non-empty parameter. We recommend that you create a new rule and delete the original rule.
+    * ## [](#)Usage notes
+    * In security group-related API documents, inbound traffic refers to the traffic that is sent by the source device and received at the destination device.
+    * Take note of the following items:
+    * *   An authorization object in a security group rule can be one of the following types: IP address or CIDR block, security group, or prefix list. You cannot call this operation to change the type of an existing authorization object. For example, if an authorization object is an IP address, you can change the authorization object to another IP address or a CIDR block, but you cannot change the authorization object to a security group or prefix list.
+    * *   You cannot change the IP address family of an existing authorization object. For example, if an authorization object is an IPv4 CIDR block, you cannot change the authorization object to an IPv6 CIDR block. If an authorization object is a prefix list of the IPv4 address family, you cannot change the authorization object to a prefix list of the IPv6 address family.
+    * *   The new security group rule after modification cannot be the same as other existing rules.
+    * *   You cannot delete the value of a non-empty parameter. If you want to delete the values of non-empty parameters, we recommend that you create another rule and delete the original rule.
     *
     * @param request ModifySecurityGroupRuleRequest
     * @param runtime runtime options for this request RuntimeOptions
@@ -80873,12 +80937,13 @@ export default class Client extends OpenApi {
   }
 
   /**
-    * In the security group-related API documents, inbound traffic refers to the traffic that is sent by the source device and received at the destination device.
-    * When you modify the rules of a security group by specifying the rule IDs, take note of the following limits:
-    * *   A security group authorization object can be one of the following types: IP address or CIDR block, security group, or prefix list. The type of an existing security group authorization object cannot be changed. If the original authorization object is an IP address, you can change it to another IP address or a CIDR block, but not to a security group or prefix list.
-    * *   The IP address family of the authorization object cannot be changed. For example, if the original authorization object is an IPv4 CIDR block, you cannot change it to an IPv6 CIDR block. If the original authorization object is a prefix list of an IPv4 address family, you cannot change it to a prefix list of an IPv6 address family.
-    * *   The modified security group rule cannot be the same as other existing rules.
-    * *   You cannot delete the value of a non-empty parameter. We recommend that you create a new rule and delete the original rule.
+    * ## [](#)Usage notes
+    * In security group-related API documents, inbound traffic refers to the traffic that is sent by the source device and received at the destination device.
+    * Take note of the following items:
+    * *   An authorization object in a security group rule can be one of the following types: IP address or CIDR block, security group, or prefix list. You cannot call this operation to change the type of an existing authorization object. For example, if an authorization object is an IP address, you can change the authorization object to another IP address or a CIDR block, but you cannot change the authorization object to a security group or prefix list.
+    * *   You cannot change the IP address family of an existing authorization object. For example, if an authorization object is an IPv4 CIDR block, you cannot change the authorization object to an IPv6 CIDR block. If an authorization object is a prefix list of the IPv4 address family, you cannot change the authorization object to a prefix list of the IPv6 address family.
+    * *   The new security group rule after modification cannot be the same as other existing rules.
+    * *   You cannot delete the value of a non-empty parameter. If you want to delete the values of non-empty parameters, we recommend that you create another rule and delete the original rule.
     *
     * @param request ModifySecurityGroupRuleRequest
     * @return ModifySecurityGroupRuleResponse
