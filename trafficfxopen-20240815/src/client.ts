85702aebbcd0f36fedb8487450b80c98ede98b86
sdk-team <sdk-team@alibabacloud.com>
2024-08-22 12:34:41 +0000

    Generated ts 2024-08-15 for TrafficFxOpen.

---
+++ trafficfxopen-20240815/src/client.ts
@@ -0,0 +1,259 @@
+// This file is auto-generated, don't edit it
+/**
+ */
+import Util, * as $Util from '@alicloud/tea-util';
+import OpenApi, * as $OpenApi from '@alicloud/openapi-client';
+import OpenApiUtil from '@alicloud/openapi-util';
+import EndpointUtil from '@alicloud/endpoint-util';
+import * as $tea from '@alicloud/tea-typescript';
+
+export class GetTokenRequest extends $tea.Model {
+  /**
+   * @remarks
+   * This parameter is required.
+   */
+  appKey?: string;
+  /**
+   * @remarks
+   * This parameter is required.
+   */
+  appSecret?: string;
+  static names(): { [key: string]: string } {
+    return {
+      appKey: 'appKey',
+      appSecret: 'appSecret',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      appKey: 'string',
+      appSecret: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetTokenResponseBody extends $tea.Model {
+  data?: GetTokenResponseBodyData;
+  /**
+   * @example
+   * success
+   */
+  errorCode?: string;
+  errorData?: GetTokenResponseBodyErrorData;
+  errorMsg?: string;
+  /**
+   * @remarks
+   * Id of the request
+   * 
+   * @example
+   * 6BD708D6-1A8C-5CF9-85B8-D620F314F1F0
+   */
+  requestId?: string;
+  /**
+   * @example
+   * confirmed
+   */
+  status?: number;
+  /**
+   * @example
+   * True
+   */
+  success?: boolean;
+  static names(): { [key: string]: string } {
+    return {
+      data: 'data',
+      errorCode: 'errorCode',
+      errorData: 'errorData',
+      errorMsg: 'errorMsg',
+      requestId: 'requestId',
+      status: 'status',
+      success: 'success',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      data: GetTokenResponseBodyData,
+      errorCode: 'string',
+      errorData: GetTokenResponseBodyErrorData,
+      errorMsg: 'string',
+      requestId: 'string',
+      status: 'number',
+      success: 'boolean',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetTokenResponse extends $tea.Model {
+  headers?: { [key: string]: string };
+  statusCode?: number;
+  body?: GetTokenResponseBody;
+  static names(): { [key: string]: string } {
+    return {
+      headers: 'headers',
+      statusCode: 'statusCode',
+      body: 'body',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      headers: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+      statusCode: 'number',
+      body: GetTokenResponseBody,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetTokenResponseBodyData extends $tea.Model {
+  /**
+   * @example
+   * 7200
+   */
+  expireTime?: string;
+  /**
+   * @example
+   * 1724130275
+   */
+  generateTime?: string;
+  /**
+   * @remarks
+   * token
+   * 
+   * @example
+   * eyJhbGciOiJSUzI1NiIsImtpZCI6ImRpc3RyaWJ1dGlvbl9rZXlpZCJ9.eyJqdGkiOiI2cDQwZDctSDQ0dUJicEJkYTZadzdBIiwiaWF0IjoxNzI0MzE2MzM1LCJleHAiOjE3MjQzMjM1MzUsIm5iZiI6MTcyNDMxNjI3NSwiYXBwS2V5IjoiNjE3NzgxZDQxM2FmNGRlZGFiNzkifQ.XtjSM7qVbESvt7n31RtD5Pp6854IVyGMEco4aEruMDMkrXHkcdZejyecKFx3RdsCldZPgvowc5EOl44c3JJfm6DENH4M6BRBSc90eqXYcD_xVJ9FhDWzK9O6OnkvR7HX1t-qqMdikLviM1w1G0DGMLaasvZ8MPMewL8k6NnvOSGePwUhb-m5IZ14VYv7BPO2dp8Jh00qNSJQrmNiWWzJUtK_xllNr3LKQ7cIVtPgFUckvRDw9Hal5oACXSRdkZFOAGlFSjpB_BbTpe5vc-AJ8K89nRD53sIy9YyVQClV_HH7PrXxFFJgReGvNsnP1h9gf55q86IzOQBkj9vGm2zXdw
+   */
+  token?: string;
+  static names(): { [key: string]: string } {
+    return {
+      expireTime: 'expireTime',
+      generateTime: 'generateTime',
+      token: 'token',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      expireTime: 'string',
+      generateTime: 'string',
+      token: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetTokenResponseBodyErrorData extends $tea.Model {
+  /**
+   * @example
+   * 1001
+   */
+  rawErrorCode?: string;
+  rawErrorMsg?: string;
+  static names(): { [key: string]: string } {
+    return {
+      rawErrorCode: 'rawErrorCode',
+      rawErrorMsg: 'rawErrorMsg',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      rawErrorCode: 'string',
+      rawErrorMsg: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+
+export default class Client extends OpenApi {
+
+  constructor(config: $OpenApi.Config) {
+    super(config);
+    this._endpointRule = "";
+    this.checkConfig(config);
+    this._endpoint = this.getEndpoint("trafficfxopen", this._regionId, this._endpointRule, this._network, this._suffix, this._endpointMap, this._endpoint);
+  }
+
+
+  getEndpoint(productId: string, regionId: string, endpointRule: string, network: string, suffix: string, endpointMap: {[key: string ]: string}, endpoint: string): string {
+    if (!Util.empty(endpoint)) {
+      return endpoint;
+    }
+
+    if (!Util.isUnset(endpointMap) && !Util.empty(endpointMap[regionId])) {
+      return endpointMap[regionId];
+    }
+
+    return EndpointUtil.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+  }
+
+  /**
+   * 创建token
+   * 
+   * @param request - GetTokenRequest
+   * @param headers - map
+   * @param runtime - runtime options for this request RuntimeOptions
+   * @returns GetTokenResponse
+   */
+  async getTokenWithOptions(request: GetTokenRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<GetTokenResponse> {
+    Util.validateModel(request);
+    let query : {[key: string ]: any} = { };
+    if (!Util.isUnset(request.appKey)) {
+      query["appKey"] = request.appKey;
+    }
+
+    if (!Util.isUnset(request.appSecret)) {
+      query["appSecret"] = request.appSecret;
+    }
+
+    let req = new $OpenApi.OpenApiRequest({
+      headers: headers,
+      query: OpenApiUtil.query(query),
+    });
+    let params = new $OpenApi.Params({
+      action: "GetToken",
+      version: "2024-08-15",
+      protocol: "HTTPS",
+      pathname: `/v1/distribution/trade/getToken`,
+      method: "GET",
+      authType: "AK",
+      style: "ROA",
+      reqBodyType: "json",
+      bodyType: "json",
+    });
+    return $tea.cast<GetTokenResponse>(await this.callApi(params, req, runtime), new GetTokenResponse({}));
+  }
+
+  /**
+   * 创建token
+   * 
+   * @param request - GetTokenRequest
+   * @returns GetTokenResponse
+   */
+  async getToken(request: GetTokenRequest): Promise<GetTokenResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.getTokenWithOptions(request, headers, runtime);
+  }
+
+}
