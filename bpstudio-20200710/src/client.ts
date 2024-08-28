8783f145b599f65577ffa3acaab9da99aeff6079
sdk-team <sdk-team@alibabacloud.com>
2024-08-28 17:16:47 +0000

    Update API GetDeployDetail: update response param.

--- bpstudio-20200710/src/client.ts
+++ bpstudio-20200710/src/client.ts
@@ -1,6 +1,5 @@
 // This file is auto-generated, don't edit it
 /**
- *
  */
 import Util, * as $Util from '@alicloud/tea-util';
 import OpenApi, * as $OpenApi from '@alicloud/openapi-client';
@@ -62,9 +61,9 @@ export class BillingApplicationResponseBody extends $tea.Model {
 }
 
 export class BillingApplicationResponse extends $tea.Model {
-  headers: { [key: string]: string };
-  statusCode: number;
-  body: BillingApplicationResponseBody;
+  headers?: { [key: string]: string };
+  statusCode?: number;
+  body?: BillingApplicationResponseBody;
   static names(): { [key: string]: string } {
     return {
       headers: 'headers',
@@ -87,13 +86,54 @@ export class BillingApplicationResponse extends $tea.Model {
 }
 
 export class GetDeployDetailRequest extends $tea.Model {
+  /**
+   * @remarks
+   * This parameter is required.
+   * 
+   * @example
+   * 30GRJUY95TMYWBYJ
+   */
   appId?: string;
+  /**
+   * @remarks
+   * This parameter is required.
+   * 
+   * @example
+   * 40
+   */
   maxResults?: number;
+  /**
+   * @remarks
+   * This parameter is required.
+   * 
+   * @example
+   * 1
+   */
   nextToken?: number;
+  /**
+   * @example
+   * 1713597738
+   */
   refId?: string;
+  /**
+   * @example
+   * rg-aekz44tg3bnpyba
+   */
   resourceGroupId?: string;
+  /**
+   * @example
+   * vsw-xxxxxxxxxxxx
+   */
   resourceId?: string;
+  /**
+   * @example
+   * rgm-ecs
+   */
   resourceName?: string;
+  /**
+   * @example
+   * ecs
+   */
   resourceType?: string;
   static names(): { [key: string]: string } {
     return {
@@ -127,11 +167,31 @@ export class GetDeployDetailRequest extends $tea.Model {
 }
 
 export class GetDeployDetailResponseBody extends $tea.Model {
+  /**
+   * @example
+   * 200
+   */
   code?: number;
   data?: GetDeployDetailResponseBodyData[];
+  /**
+   * @example
+   * Success
+   */
   message?: string;
+  /**
+   * @example
+   * 0
+   */
   nextToken?: number;
+  /**
+   * @example
+   * E15B71B4-5D8F-5484-BC07-989E2987EE7C
+   */
   requestId?: string;
+  /**
+   * @example
+   * 10
+   */
   totalCount?: number;
   static names(): { [key: string]: string } {
     return {
@@ -161,9 +221,9 @@ export class GetDeployDetailResponseBody extends $tea.Model {
 }
 
 export class GetDeployDetailResponse extends $tea.Model {
-  headers: { [key: string]: string };
-  statusCode: number;
-  body: GetDeployDetailResponseBody;
+  headers?: { [key: string]: string };
+  statusCode?: number;
+  body?: GetDeployDetailResponseBody;
   static names(): { [key: string]: string } {
     return {
       headers: 'headers',
@@ -186,6 +246,10 @@ export class GetDeployDetailResponse extends $tea.Model {
 }
 
 export class GetDeployDetailResponseBodyDataResourceListOperation extends $tea.Model {
+  /**
+   * @example
+   * ecsDelete
+   */
   name?: string;
   operations?: { [key: string]: any };
   static names(): { [key: string]: string } {
@@ -207,19 +271,92 @@ export class GetDeployDetailResponseBodyDataResourceListOperation extends $tea.M
   }
 }
 
+export class GetDeployDetailResponseBodyDataResourceListResourceTimeList extends $tea.Model {
+  bizId?: string;
+  creationEndTime?: number;
+  creationStartTime?: number;
+  id?: number;
+  static names(): { [key: string]: string } {
+    return {
+      bizId: 'BizId',
+      creationEndTime: 'CreationEndTime',
+      creationStartTime: 'CreationStartTime',
+      id: 'id',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      bizId: 'string',
+      creationEndTime: 'number',
+      creationStartTime: 'number',
+      id: 'number',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class GetDeployDetailResponseBodyDataResourceList extends $tea.Model {
   buyDuration?: string;
+  /**
+   * @example
+   * Free
+   */
   chargeType?: string;
+  /**
+   * @example
+   * OpenApi
+   */
   executionStrategy?: string;
+  /**
+   * @example
+   * 1714031840000
+   */
   modifiedTime?: number;
+  /**
+   * @example
+   * https://cloudmonitor.console.aliyun.com/?#/generalcharts/product=vpc&group=&type=&return&region=cn-hangzhou&dimension=instanceId:vpc-bp1n6uc5jqxtff2euhnx5
+   */
   monitorURL?: string;
+  /**
+   * @example
+   * vpc
+   */
   nodeName?: string;
   operation?: GetDeployDetailResponseBodyDataResourceListOperation;
+  /**
+   * @example
+   * 1716049421
+   */
   refId?: number;
+  /**
+   * @example
+   * {\\"Description\\":\\"\\",\\"ClassicLinkEnabled\\":false,\\"ResourceGroupId\\":\\"rg-acfm4mlwqjalz7a\\",\\"SecondaryCidrBlocks\\":[],\\"CidrBlock\\":\\"192.168.0.0/16\\",\\"UserCidrs\\":[],\\"NetworkAclNum\\":\\"0\\",\\"VRouterId\\":\\"vrt-m5ek7vcyld0x5ym8m9hix\\",\\"OwnerId\\":1986207497633020,\\"AssociatedCens\\":[],\\"id\\":\\"vpc-m5e3c9nz4lj19byan9m2g\\",\\"CloudResources\\":[{\\"ResourceCount\\":1,\\"ResourceType\\":\\"VSwitch\\"},{\\"ResourceCount\\":1,\\"ResourceType\\":\\"VRouter\\"},{\\"ResourceCount\\":1,\\"ResourceType\\":\\"RouteTable\\"}],\\"Tags\\":[],\\"Status\\":\\"Available\\",\\"IsDefault\\":false,\\"RequestId\\":\\"494646FB-14C0-5B4C-9684-B6EFBF4DF01C\\",\\"SupportIpv4Gateway\\":false,\\"Ipv4GatewayId\\":\\"\\",\\"VSwitchIds\\":[\\"vsw-m5egl9wtppiadysjwlgb1\\"],\\"VpcId\\":\\"vpc-m5e3c9nz4lj19byan9m2g\\",\\"CreationTime\\":\\"2024-04-25T07:56:59Z\\",\\"VpcName\\":\\"vpc\\",\\"refId\\":\\"1714031764_0\\",\\"RegionId\\":\\"cn-qingdao\\",\\"Ipv6CidrBlock\\":\\"\\",\\"Ipv6CidrBlocks\\":[]}
+   */
   remark?: string;
+  /**
+   * @example
+   * ecs
+   */
   resourceCode?: string;
+  /**
+   * @example
+   * i-2zehnzxqixu1pywsfbx1
+   */
   resourceId?: string;
+  resourceTimeList?: GetDeployDetailResponseBodyDataResourceListResourceTimeList[];
+  /**
+   * @example
+   * vpc
+   */
   resourceType?: string;
+  /**
+   * @example
+   * Finish
+   */
   status?: string;
   static names(): { [key: string]: string } {
     return {
@@ -234,6 +371,7 @@ export class GetDeployDetailResponseBodyDataResourceList extends $tea.Model {
       remark: 'Remark',
       resourceCode: 'ResourceCode',
       resourceId: 'ResourceId',
+      resourceTimeList: 'ResourceTimeList',
       resourceType: 'ResourceType',
       status: 'Status',
     };
@@ -252,6 +390,7 @@ export class GetDeployDetailResponseBodyDataResourceList extends $tea.Model {
       remark: 'string',
       resourceCode: 'string',
       resourceId: 'string',
+      resourceTimeList: { 'type': 'array', 'itemType': GetDeployDetailResponseBodyDataResourceListResourceTimeList },
       resourceType: 'string',
       status: 'string',
     };
@@ -263,21 +402,57 @@ export class GetDeployDetailResponseBodyDataResourceList extends $tea.Model {
 }
 
 export class GetDeployDetailResponseBodyData extends $tea.Model {
+  /**
+   * @example
+   * EQ4W772D0VJ33IV1
+   */
   appId?: string;
+  /**
+   * @example
+   * 1645516991000
+   */
   createTime?: number;
+  /**
+   * @example
+   * DEPLOY
+   */
   currentProcess?: string;
   deletingNodeList?: { [key: string]: any }[];
+  /**
+   * @example
+   * 0
+   */
   deployPercent?: number;
   deployedNodeList?: { [key: string]: any }[];
   deployingNodeList?: { [key: string]: any }[];
+  /**
+   * @example
+   * OperationNotSupport.SubscriptionCommodityCanNotDelete
+   */
   error?: string;
   executionTime?: number;
   failStatus?: number;
   orderIdList?: string[];
+  /**
+   * @example
+   * https://cadt-studio-publish.oss-cn-hangzhou.aliyuncs.com/1986207497633020/deployReport-EXN4FNUR12M35KJM.pdf?Expires=1716967763&OSSAccessKeyId=********nw4rvYAweFuQc3&Signature=*******fKo6164wykT9jBOsSGeQ%3D
+   */
   pdfUrl?: string;
+  /**
+   * @example
+   * rg-aekzhfgmw4e6fwq
+   */
   resourceGroupId?: string;
   resourceList?: GetDeployDetailResponseBodyDataResourceList[];
+  /**
+   * @example
+   * Revised
+   */
   status?: string;
+  /**
+   * @example
+   * https://cadt-studio-publish.oss-cn-hangzhou.aliyuncs.com/1986207497633020/app_EXN4FNUR12M35KJM.tf?Expires=1716967763&OSSAccessKeyId=*******nw4rvYAweFuQc3&Signature=%2********lYROqJLNvyA8g6qD9U%3D
+   */
   terraformScriptUrl?: string;
   static names(): { [key: string]: string } {
     return {
@@ -349,6 +524,13 @@ export default class Client extends OpenApi {
     return EndpointUtil.getEndpointRules(productId, regionId, endpointRule, network, suffix);
   }
 
+  /**
+   * BillingApplication
+   * 
+   * @param request - BillingApplicationRequest
+   * @param runtime - runtime options for this request RuntimeOptions
+   * @returns BillingApplicationResponse
+   */
   async billingApplicationWithOptions(request: BillingApplicationRequest, runtime: $Util.RuntimeOptions): Promise<BillingApplicationResponse> {
     Util.validateModel(request);
     let body : {[key: string ]: any} = { };
@@ -381,11 +563,24 @@ export default class Client extends OpenApi {
     return $tea.cast<BillingApplicationResponse>(await this.callApi(params, req, runtime), new BillingApplicationResponse({}));
   }
 
+  /**
+   * BillingApplication
+   * 
+   * @param request - BillingApplicationRequest
+   * @returns BillingApplicationResponse
+   */
   async billingApplication(request: BillingApplicationRequest): Promise<BillingApplicationResponse> {
     let runtime = new $Util.RuntimeOptions({ });
     return await this.billingApplicationWithOptions(request, runtime);
   }
 
+  /**
+   * 分页查询部署清单
+   * 
+   * @param request - GetDeployDetailRequest
+   * @param runtime - runtime options for this request RuntimeOptions
+   * @returns GetDeployDetailResponse
+   */
   async getDeployDetailWithOptions(request: GetDeployDetailRequest, runtime: $Util.RuntimeOptions): Promise<GetDeployDetailResponse> {
     Util.validateModel(request);
     let body : {[key: string ]: any} = { };
@@ -438,6 +633,12 @@ export default class Client extends OpenApi {
     return $tea.cast<GetDeployDetailResponse>(await this.callApi(params, req, runtime), new GetDeployDetailResponse({}));
   }
 
+  /**
+   * 分页查询部署清单
+   * 
+   * @param request - GetDeployDetailRequest
+   * @returns GetDeployDetailResponse
+   */
   async getDeployDetail(request: GetDeployDetailRequest): Promise<GetDeployDetailResponse> {
     let runtime = new $Util.RuntimeOptions({ });
     return await this.getDeployDetailWithOptions(request, runtime);
