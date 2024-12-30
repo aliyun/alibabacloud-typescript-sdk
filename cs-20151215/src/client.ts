b19fd03709274812793b6c157704e84d29575d61
sdk-team <sdk-team@alibabacloud.com>
2024-12-30 17:16:42 +0000

    Update API ListOperationPlans: update response param.

--- cs-20151215/src/client.ts
+++ cs-20151215/src/client.ts
@@ -11457,7 +11457,7 @@ export class MigrateClusterResponse extends $tea.Model {
 export class ModifyClusterRequest extends $tea.Model {
   /**
    * @remarks
-   * The network access control lists (ACLs) of the SLB instance associated with the API server if the cluster is a registered cluster.
+   * The network access control list (ACL) of the SLB instance associated with the API server if the cluster is a registered cluster.
    */
   accessControlList?: string[];
   /**
@@ -11494,15 +11494,19 @@ export class ModifyClusterRequest extends $tea.Model {
    * cluster-new-name
    */
   clusterName?: string;
+  /**
+   * @remarks
+   * The control plane configurations of an ACK dedicated cluster.
+   */
   controlPlaneConfig?: ModifyClusterRequestControlPlaneConfig;
   /**
    * @remarks
-   * Specifies whether to enable cluster deletion protection. If this option is enabled, the cluster cannot be deleted in the console or by calling API operations. Valid values:
+   * Specifies whether to enable cluster deletion protection. If you enable this option, the cluster cannot be deleted in the console or by calling API operations. Valid values:
    * 
    * *   `true`: enables cluster deletion protection.
    * *   `false`: disables cluster deletion protection.
    * 
-   * Default value: `false`
+   * Default value: `false`.
    * 
    * @example
    * true
@@ -11526,7 +11530,7 @@ export class ModifyClusterRequest extends $tea.Model {
    * *   `true`: remaps the test domain name of the cluster.
    * *   `false`: does not remap the test domain name of the cluster.
    * 
-   * Default value: `false`
+   * Default value: `false`.
    * 
    * @example
    * true
@@ -11547,7 +11551,7 @@ export class ModifyClusterRequest extends $tea.Model {
    * *   `true`: enables instance deletion protection.
    * *   `false`: disables instance deletion protection.
    * 
-   * Default value: `false`
+   * Default value: `false`.
    * 
    * @example
    * true
@@ -11565,7 +11569,7 @@ export class ModifyClusterRequest extends $tea.Model {
   operationPolicy?: ModifyClusterRequestOperationPolicy;
   /**
    * @remarks
-   * The cluster resource group ID.
+   * The resource group ID of the cluster.
    * 
    * @example
    * rg-acfmyvw3wjm****
@@ -11578,12 +11582,12 @@ export class ModifyClusterRequest extends $tea.Model {
   systemEventsLogging?: ModifyClusterRequestSystemEventsLogging;
   /**
    * @remarks
-   * The vSwitches of the control planes. This parameter can be used to change the vSwitches of the control planes in an ACK managed cluster. Take note of the following items:
+   * The vSwitches of the control plane. This parameter can be used to change the vSwitches of the control plane in an ACK managed cluster. Take note of the following items:
    * 
-   * *   This parameter overwrites the existing configuration. You must specify all vSwitches of the control planes.
-   * *   The control planes restart during the update process. Exercise caution when you perform this operation.
-   * *   Make sure that all security groups of the cluster, including the security groups of the control planes, all node pools, and container network, are allowed to access the CIDR blocks of the new vSwitches. This ensures that the nodes and containers can connect to the API server.
-   * *   If the new vSwitches of the control planes are configured with an access control list (ACL), ensure that the ACL allows communication between the new vSwitches and the CIDR blocks of cluster nodes and container networks.
+   * *   This parameter overwrites the existing configuration. You must specify all vSwitches of the control plane.
+   * *   The control plane restarts during the change process. Exercise caution when you perform this operation.
+   * *   Ensure that all security groups of the cluster, including the security groups of the control plane, all node pools, and container network, are allowed to access the CIDR blocks of the new vSwitches. This ensures that the nodes and containers can connect to the API server.
+   * *   If the new vSwitches of the control plane are configured with an ACL, ensure that the ACL allows communication between the new vSwitches and CIDR blocks such as those of the cluster nodes and the container network.
    */
   vswitchIds?: string[];
   static names(): { [key: string]: string } {
@@ -26862,6 +26866,7 @@ export class ListOperationPlansResponseBodyPlans extends $tea.Model {
    * cluster
    */
   targetType?: string;
+  taskId?: string;
   /**
    * @remarks
    * The plan type. Valid values:
@@ -26882,6 +26887,7 @@ export class ListOperationPlansResponseBodyPlans extends $tea.Model {
       state: 'state',
       targetId: 'target_id',
       targetType: 'target_type',
+      taskId: 'task_id',
       type: 'type',
     };
   }
@@ -26896,6 +26902,7 @@ export class ListOperationPlansResponseBodyPlans extends $tea.Model {
       state: 'string',
       targetId: 'string',
       targetType: 'string',
+      taskId: 'string',
       type: 'string',
     };
   }
@@ -27129,7 +27136,7 @@ export class ModifyClusterRequestApiServerCustomCertSans extends $tea.Model {
   action?: string;
   /**
    * @remarks
-   * The SANs.
+   * The list of SANs.
    */
   subjectAlternativeNames?: string[];
   static names(): { [key: string]: string } {
@@ -27152,29 +27159,251 @@ export class ModifyClusterRequestApiServerCustomCertSans extends $tea.Model {
 }
 
 export class ModifyClusterRequestControlPlaneConfig extends $tea.Model {
+  /**
+   * @remarks
+   * Specifies whether to enable auto-renewal for the instance. This parameter takes effect only when `charge_type` is set to `PrePaid`. Valid values:
+   * 
+   * *   `true`: enables auto-renewal.
+   * *   `false`: disables auto-renewal.
+   * 
+   * Default value: `false`.
+   * 
+   * @example
+   * true
+   */
   autoRenew?: boolean;
+  /**
+   * @remarks
+   * The auto-renewal period of the instance. Valid values: 1, 2, 3, 6, and 12.
+   * 
+   * Default value: 1.
+   * 
+   * @example
+   * 1
+   */
   autoRenewPeriod?: number;
+  /**
+   * @remarks
+   * The billing method of the instance. Valid values:
+   * 
+   * *   `PrePaid`: subscription.
+   * *   `PostPaid`: pay-as-you-go.
+   * 
+   * Default value: `PostPaid`.
+   * 
+   * @example
+   * PrePaid
+   */
   chargeType?: string;
+  /**
+   * @remarks
+   * Specifies whether to install the CloudMonitor agent. Valid values:
+   * 
+   * *   `true`: installs the CloudMonitor agent.
+   * *   `false`: does not install the CloudMonitor agent.
+   * 
+   * @example
+   * true
+   */
   cloudMonitorFlags?: boolean;
+  /**
+   * @remarks
+   * The CPU management policy of nodes in the node pool. The following policies are supported if the Kubernetes version of the cluster is 1.12.6 or later:
+   * 
+   * *   `static`: allows pods with specific resource characteristics on the node to be granted with enhanced CPU affinity and exclusivity.
+   * *   `none`: specifies that the default CPU affinity is used.
+   * 
+   * Default value: `none`.
+   * 
+   * @example
+   * none
+   */
   cpuPolicy?: string;
+  /**
+   * @remarks
+   * The ID of the deployment set.
+   * 
+   * @example
+   * ds-bp10b35imuam5amw****
+   */
   deploymentsetId?: string;
+  /**
+   * @remarks
+   * The custom image ID. You must configure this parameter if you use a custom image.
+   * 
+   * @example
+   * aliyun_3_x64_20G_alibase_20240819.vhd
+   */
   imageId?: string;
+  /**
+   * @remarks
+   * The type of the OS image. Valid values:
+   * 
+   * *   `AliyunLinux3`: Alibaba Cloud Linux 3.
+   * *   `Custom`: the custom image.
+   * 
+   * @example
+   * AliyunLinux3
+   */
   imageType?: string;
+  /**
+   * @remarks
+   * The instance type. For more information, see [Overview of ECS instance families](https://help.aliyun.com/document_detail/25378.html).
+   */
   instanceTypes?: string[];
+  /**
+   * @remarks
+   * The name of the key pair. You must configure either this parameter or the `login_password` parameter.
+   * 
+   * @example
+   * ack
+   */
   keyPair?: string;
+  /**
+   * @remarks
+   * The password for SSH logon. You must configure either this parameter or the `key_pair` parameter. The password must be 8 to 30 characters in length, and must contain at least three of the following character types: uppercase letters, lowercase letters, digits, and special characters. To log on with a password, you must specify this parameter during the scale-out.
+   * 
+   * @example
+   * Ack@2000.
+   */
   loginPassword?: string;
+  /**
+   * @remarks
+   * The node port range.
+   * 
+   * @example
+   * 30000-32767
+   */
   nodePortRange?: string;
+  /**
+   * @remarks
+   * The subscription duration of the instance. This parameter takes effect and is required only when `charge_type` is set to `PrePaid`.
+   * 
+   * If `PeriodUnit=Month` is specified, the valid values are 1, 2, 3, 6, 12, 24, 36, 48, and 60.
+   * 
+   * @example
+   * 1
+   */
   period?: number;
+  /**
+   * @remarks
+   * The billing cycle of the instance. This parameter takes effect only when `instance_charge_type` is set to `PrePaid`.
+   * 
+   * Valid value: `Month`.
+   * 
+   * @example
+   * Month
+   */
   periodUnit?: string;
+  /**
+   * @remarks
+   * The type of the container runtime. Valid values:
+   * 
+   * *   `containerd`: supports all Kubernetes versions. We recommend that you set the parameter to this value.
+   * 
+   * Default value: containerd.
+   * 
+   * @example
+   * containerd
+   */
   runtime?: string;
+  /**
+   * @remarks
+   * Specifies whether to enable Alibaba Cloud Linux Security Hardening. Valid values:
+   * 
+   * *   `true`: enables Alibaba Cloud Linux Security Hardening.
+   * *   `false`: disables Alibaba Cloud Linux Security Hardening.
+   * 
+   * Default value: `false`.
+   * 
+   * @example
+   * true
+   */
   securityHardeningOs?: boolean;
+  /**
+   * @remarks
+   * The number of control plane nodes. If you want to scale out the control plane in an ACK dedicated cluster, set this parameter to the desired number of nodes. This parameter must be greater than the current number of nodes.
+   * 
+   * @example
+   * 5
+   */
   size?: number;
+  /**
+   * @remarks
+   * Specifies whether to enable Multi-Level Protection Scheme (MLPS) security hardening. For more information, see [ACK security hardening based on MLPS](https://help.aliyun.com/document_detail/196148.html).
+   * 
+   * Valid values:
+   * 
+   * *   `true`: enables MLPS security hardening.
+   * *   `false`: disables MLPS security hardening.
+   * 
+   * Default value: `false`.
+   * 
+   * @example
+   * false
+   */
   socEnabled?: boolean;
+  /**
+   * @remarks
+   * Specifies whether to enable the burst feature for the system disk. Valid values:
+   * 
+   * *   `true`: enables the burst feature.
+   * *   `false`: disables the burst feature.
+   * 
+   * This parameter is effective only when `system_disk_category` is set to `cloud_auto`. For more information, see [ESSD AutoPL disks](https://help.aliyun.com/document_detail/368372.html).
+   * 
+   * @example
+   * true
+   */
   systemDiskBurstingEnabled?: boolean;
+  /**
+   * @remarks
+   * The type of the node system disk. Valid values:
+   * 
+   * *   `cloud`: basic disk.
+   * *   `cloud_efficiency`: ultra disk.
+   * *   `cloud_ssd`: standard SSD.
+   * *   `cloud_essd`: enhanced SSD (ESSD).
+   * *   `cloud_auto`: ESSD AutoPL disk.
+   * *   `cloud_essd_entry`: ESSD Entry disk.
+   * 
+   * @example
+   * cloud_essd
+   */
   systemDiskCategory?: string;
+  /**
+   * @remarks
+   * The performance level (PL) of the system disk that you want to use for the node. This parameter is effective only for ESSDs. This parameter is related to the disk size. For more information, see [ESSDs](https://help.aliyun.com/document_detail/122389.html).
+   * 
+   * @example
+   * PL1
+   */
   systemDiskPerformanceLevel?: string;
+  /**
+   * @remarks
+   * The preset read/write input/output operations per second (IOPS) of the system disk. Valid values: 0 to min{50,000, 1,000 × Capacity - Baseline IOPS} Baseline IOPS = min{1,800 + 50 × Capacity, 50,000}.
+   * 
+   * This parameter is effective only when `system_disk_category` is set to `cloud_auto`. For more information, see [ESSD AutoPL disks](https://help.aliyun.com/document_detail/368372.html).
+   * 
+   * @example
+   * 1000
+   */
   systemDiskProvisionedIops?: number;
+  /**
+   * @remarks
+   * The type of the system disk. Valid values: [40,500]. Unit: GiB.
+   * 
+   * @example
+   * 120
+   */
   systemDiskSize?: number;
+  /**
+   * @remarks
+   * The ID of the automatic snapshot policy applied to the node system disk.
+   * 
+   * @example
+   * sp-2zej1nogjvovnz4z****
+   */
   systemDiskSnapshotPolicyId?: string;
   static names(): { [key: string]: string } {
     return {
@@ -27242,11 +27471,11 @@ export class ModifyClusterRequestControlPlaneConfig extends $tea.Model {
 export class ModifyClusterRequestOperationPolicyClusterAutoUpgrade extends $tea.Model {
   /**
    * @remarks
-   * The automatic update frequency. For more information, see [Update frequency](https://help.aliyun.com/document_detail/2712866.html).
+   * The frequency of automatic cluster updates. For more information, see [Update frequency](https://help.aliyun.com/document_detail/2712866.html).
    * 
    * Valid values:
    * 
-   * *   patch: specifies the latest patch version.
+   * *   patch: the latest patch version.
    * *   stables: the second-latest minor version.
    * *   rapid: the latest minor version.
    * 
