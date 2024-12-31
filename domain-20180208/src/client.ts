8a7b69de2028e0d002680ee3bf59218a30d86896
sdk-team <sdk-team@alibabacloud.com>
2024-12-31 17:14:25 +0000

    Update API CreateFixedPriceSelectedOrder: update response param.

--- domain-20180208/src/client.ts
+++ domain-20180208/src/client.ts
@@ -3095,6 +3095,7 @@ export class CreateFixedPriceSelectedOrderResponseBodyModule extends $tea.Model 
    * example.com
    */
   domain?: string;
+  domainBlockTrade?: string[];
   /**
    * @example
    * 31199295f2074ce895645d386cb22c36
@@ -3108,6 +3109,7 @@ export class CreateFixedPriceSelectedOrderResponseBodyModule extends $tea.Model 
   static names(): { [key: string]: string } {
     return {
       domain: 'Domain',
+      domainBlockTrade: 'DomainBlockTrade',
       orderNo: 'OrderNo',
       price: 'Price',
     };
@@ -3116,6 +3118,7 @@ export class CreateFixedPriceSelectedOrderResponseBodyModule extends $tea.Model 
   static types(): { [key: string]: any } {
     return {
       domain: 'string',
+      domainBlockTrade: { 'type': 'array', 'itemType': 'string' },
       orderNo: 'string',
       price: 'number',
     };
