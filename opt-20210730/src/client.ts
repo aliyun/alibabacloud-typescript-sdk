84ef1f2418d84970cc742f3cede6ed449f1b0e49
sdk-team <sdk-team@alibabacloud.com>
2025-02-07 17:14:05 +0000

    Update API GetOrderInfo: update response param.

--- opt-20210730/src/client.ts
+++ opt-20210730/src/client.ts
@@ -1,18 +1,79 @@
 // This file is auto-generated, don't edit it
 /**
- *
  */
-import Util, * as $Util from '@alicloud/tea-util';
-import OpenApi, * as $OpenApi from '@alicloud/openapi-client';
-import OpenApiUtil from '@alicloud/openapi-util';
-import EndpointUtil from '@alicloud/endpoint-util';
-import * as $tea from '@alicloud/tea-typescript';
+import OpenApi from '@alicloud/openapi-core';
+import { OpenApiUtil, $OpenApiUtil } from '@alicloud/openapi-core';
+import * as $dara from '@darabonba/typescript';
 
-export class GetOpenStatusResponseBody extends $tea.Model {
+export class GetOrderInfoResponseBodyData extends $dara.Model {
+  bizType?: string;
+  currentConcurrency?: number;
+  currentDays?: number;
+  instanceId?: string;
+  isReleased?: boolean;
+  licenseKey?: string;
+  remark?: string;
+  totalDays?: number;
+  static names(): { [key: string]: string } {
+    return {
+      bizType: 'bizType',
+      currentConcurrency: 'currentConcurrency',
+      currentDays: 'currentDays',
+      instanceId: 'instanceId',
+      isReleased: 'isReleased',
+      licenseKey: 'licenseKey',
+      remark: 'remark',
+      totalDays: 'totalDays',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      bizType: 'string',
+      currentConcurrency: 'number',
+      currentDays: 'number',
+      instanceId: 'string',
+      isReleased: 'boolean',
+      licenseKey: 'string',
+      remark: 'string',
+      totalDays: 'number',
+    };
+  }
+
+  validate() {
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetOpenStatusResponseBody extends $dara.Model {
+  /**
+   * @example
+   * 0
+   */
   code?: number;
+  /**
+   * @example
+   * {"gmtModified":"2021-07-27T04:00:00.000+00:00","mpStatus":1,"id":11,"pk":"1084126944995576","gmtCreate":"2021-07-27T04:00:00.000+00:00","parentPk":"1084126944995576"}
+   */
   data?: { [key: string]: any };
+  /**
+   * @example
+   * success
+   */
   message?: string;
+  /**
+   * @example
+   * 0EB-FCAC-1B78-BBB8-500ED951E9EB
+   */
   requestId?: string;
+  /**
+   * @example
+   * true
+   */
   success?: boolean;
   static names(): { [key: string]: string } {
     return {
@@ -34,12 +95,19 @@ export class GetOpenStatusResponseBody extends $tea.Model {
     };
   }
 
+  validate() {
+    if(this.data) {
+      $dara.Model.validateMap(this.data);
+    }
+    super.validate();
+  }
+
   constructor(map?: { [key: string]: any }) {
     super(map);
   }
 }
 
-export class GetOpenStatusResponse extends $tea.Model {
+export class GetOpenStatusResponse extends $dara.Model {
   headers?: { [key: string]: string };
   statusCode?: number;
   body?: GetOpenStatusResponseBody;
@@ -59,14 +127,38 @@ export class GetOpenStatusResponse extends $tea.Model {
     };
   }
 
+  validate() {
+    if(this.headers) {
+      $dara.Model.validateMap(this.headers);
+    }
+    if(this.body && typeof (this.body as any).validate === 'function') {
+      (this.body as any).validate();
+    }
+    super.validate();
+  }
+
   constructor(map?: { [key: string]: any }) {
     super(map);
   }
 }
 
-export class GetOrderInfoRequest extends $tea.Model {
+export class GetOrderInfoRequest extends $dara.Model {
   listReleased?: boolean;
+  /**
+   * @remarks
+   * This parameter is required.
+   * 
+   * @example
+   * MP
+   */
   relService?: string;
+  /**
+   * @remarks
+   * This parameter is required.
+   * 
+   * @example
+   * 1
+   */
   resourceType?: number;
   static names(): { [key: string]: string } {
     return {
@@ -84,16 +176,40 @@ export class GetOrderInfoRequest extends $tea.Model {
     };
   }
 
+  validate() {
+    super.validate();
+  }
+
   constructor(map?: { [key: string]: any }) {
     super(map);
   }
 }
 
-export class GetOrderInfoResponseBody extends $tea.Model {
+export class GetOrderInfoResponseBody extends $dara.Model {
+  /**
+   * @example
+   * 0
+   */
   code?: string;
+  /**
+   * @example
+   * {"licenseKey":"eems7ri3b1u5nui*****","currentConcurrency":2,"instanceId":"opt_mplicense_public_cn-****","totalDays":124,"currentDays":103}
+   */
   data?: GetOrderInfoResponseBodyData[];
+  /**
+   * @example
+   * success
+   */
   message?: string;
+  /**
+   * @example
+   * 5D6653C5-CA2B-14EC-9CF0-50AA0FF49C31
+   */
   requestId?: string;
+  /**
+   * @example
+   * true
+   */
   success?: boolean;
   static names(): { [key: string]: string } {
     return {
@@ -115,12 +231,19 @@ export class GetOrderInfoResponseBody extends $tea.Model {
     };
   }
 
+  validate() {
+    if(Array.isArray(this.data)) {
+      $dara.Model.validateArray(this.data);
+    }
+    super.validate();
+  }
+
   constructor(map?: { [key: string]: any }) {
     super(map);
   }
 }
 
-export class GetOrderInfoResponse extends $tea.Model {
+export class GetOrderInfoResponse extends $dara.Model {
   headers?: { [key: string]: string };
   statusCode?: number;
   body?: GetOrderInfoResponseBody;
@@ -140,15 +263,47 @@ export class GetOrderInfoResponse extends $tea.Model {
     };
   }
 
+  validate() {
+    if(this.headers) {
+      $dara.Model.validateMap(this.headers);
+    }
+    if(this.body && typeof (this.body as any).validate === 'function') {
+      (this.body as any).validate();
+    }
+    super.validate();
+  }
+
   constructor(map?: { [key: string]: any }) {
     super(map);
   }
 }
 
-export class GetOrderUsageRequest extends $tea.Model {
+export class GetOrderUsageRequest extends $dara.Model {
+  /**
+   * @remarks
+   * This parameter is required.
+   */
   licenseKey?: string;
+  /**
+   * @remarks
+   * This parameter is required.
+   * 
+   * @example
+   * MP
+   */
   relService?: string;
+  /**
+   * @remarks
+   * This parameter is required.
+   * 
+   * @example
+   * 1
+   */
   resourceType?: number;
+  /**
+   * @example
+   * 1
+   */
   timeRange?: number;
   static names(): { [key: string]: string } {
     return {
@@ -168,15 +323,31 @@ export class GetOrderUsageRequest extends $tea.Model {
     };
   }
 
+  validate() {
+    super.validate();
+  }
+
   constructor(map?: { [key: string]: any }) {
     super(map);
   }
 }
 
-export class GetOrderUsageResponseBody extends $tea.Model {
+export class GetOrderUsageResponseBody extends $dara.Model {
   data?: { [key: string]: any }[];
+  /**
+   * @example
+   * success
+   */
   message?: string;
+  /**
+   * @example
+   * 5D6653C5-CA2B-14EC-9CF0-50AA0FF49C31
+   */
   requestId?: string;
+  /**
+   * @example
+   * true
+   */
   success?: boolean;
   static names(): { [key: string]: string } {
     return {
@@ -196,12 +367,19 @@ export class GetOrderUsageResponseBody extends $tea.Model {
     };
   }
 
+  validate() {
+    if(Array.isArray(this.data)) {
+      $dara.Model.validateArray(this.data);
+    }
+    super.validate();
+  }
+
   constructor(map?: { [key: string]: any }) {
     super(map);
   }
 }
 
-export class GetOrderUsageResponse extends $tea.Model {
+export class GetOrderUsageResponse extends $dara.Model {
   headers?: { [key: string]: string };
   statusCode?: number;
   body?: GetOrderUsageResponseBody;
@@ -221,41 +399,14 @@ export class GetOrderUsageResponse extends $tea.Model {
     };
   }
 
-  constructor(map?: { [key: string]: any }) {
-    super(map);
-  }
-}
-
-export class GetOrderInfoResponseBodyData extends $tea.Model {
-  bizType?: string;
-  currentConcurrency?: number;
-  currentDays?: number;
-  instanceId?: string;
-  licenseKey?: string;
-  remark?: string;
-  totalDays?: number;
-  static names(): { [key: string]: string } {
-    return {
-      bizType: 'bizType',
-      currentConcurrency: 'currentConcurrency',
-      currentDays: 'currentDays',
-      instanceId: 'instanceId',
-      licenseKey: 'licenseKey',
-      remark: 'remark',
-      totalDays: 'totalDays',
-    };
-  }
-
-  static types(): { [key: string]: any } {
-    return {
-      bizType: 'string',
-      currentConcurrency: 'number',
-      currentDays: 'number',
-      instanceId: 'string',
-      licenseKey: 'string',
-      remark: 'string',
-      totalDays: 'number',
-    };
+  validate() {
+    if(this.headers) {
+      $dara.Model.validateMap(this.headers);
+    }
+    if(this.body && typeof (this.body as any).validate === 'function') {
+      (this.body as any).validate();
+    }
+    super.validate();
   }
 
   constructor(map?: { [key: string]: any }) {
@@ -266,7 +417,7 @@ export class GetOrderInfoResponseBodyData extends $tea.Model {
 
 export default class Client extends OpenApi {
 
-  constructor(config: $OpenApi.Config) {
+  constructor(config: $OpenApiUtil.Config) {
     super(config);
     this._endpointRule = "";
     this.checkConfig(config);
@@ -275,25 +426,25 @@ export default class Client extends OpenApi {
 
 
   getEndpoint(productId: string, regionId: string, endpointRule: string, network: string, suffix: string, endpointMap: {[key: string ]: string}, endpoint: string): string {
-    if (!Util.empty(endpoint)) {
+    if (!$dara.isNull(endpoint)) {
       return endpoint;
     }
 
-    if (!Util.isUnset(endpointMap) && !Util.empty(endpointMap[regionId])) {
+    if (!$dara.isNull(endpointMap) && !$dara.isNull(endpointMap[regionId])) {
       return endpointMap[regionId];
     }
 
-    return EndpointUtil.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+    return OpenApiUtil.getEndpointRules(productId, regionId, endpointRule, network, suffix);
   }
 
   /**
-   * @param request GetOpenStatusRequest
-   * @param runtime runtime options for this request RuntimeOptions
-   * @return GetOpenStatusResponse
+   * @param request - GetOpenStatusRequest
+   * @param runtime - runtime options for this request RuntimeOptions
+   * @returns GetOpenStatusResponse
    */
-  async getOpenStatusWithOptions(runtime: $Util.RuntimeOptions): Promise<GetOpenStatusResponse> {
-    let req = new $OpenApi.OpenApiRequest({ });
-    let params = new $OpenApi.Params({
+  async getOpenStatusWithOptions(runtime: $dara.RuntimeOptions): Promise<GetOpenStatusResponse> {
+    let req = new $OpenApiUtil.OpenApiRequest({ });
+    let params = new $OpenApiUtil.Params({
       action: "GetOpenStatus",
       version: "2021-07-30",
       protocol: "HTTPS",
@@ -304,41 +455,48 @@ export default class Client extends OpenApi {
       reqBodyType: "formData",
       bodyType: "json",
     });
-    return $tea.cast<GetOpenStatusResponse>(await this.callApi(params, req, runtime), new GetOpenStatusResponse({}));
+    if ($dara.isNull(this._signatureVersion) || this._signatureVersion != "v4") {
+      return $dara.cast<GetOpenStatusResponse>(await this.callApi(params, req, runtime), new GetOpenStatusResponse({}));
+    } else {
+      return $dara.cast<GetOpenStatusResponse>(await this.execute(params, req, runtime), new GetOpenStatusResponse({}));
+    }
+
   }
 
   /**
-   * @return GetOpenStatusResponse
+   * @returns GetOpenStatusResponse
    */
   async getOpenStatus(): Promise<GetOpenStatusResponse> {
-    let runtime = new $Util.RuntimeOptions({ });
+    let runtime = new $dara.RuntimeOptions({ });
     return await this.getOpenStatusWithOptions(runtime);
   }
 
   /**
-   * @param request GetOrderInfoRequest
-   * @param runtime runtime options for this request RuntimeOptions
-   * @return GetOrderInfoResponse
+   * 获取在线license列表
+   * 
+   * @param request - GetOrderInfoRequest
+   * @param runtime - runtime options for this request RuntimeOptions
+   * @returns GetOrderInfoResponse
    */
-  async getOrderInfoWithOptions(request: GetOrderInfoRequest, runtime: $Util.RuntimeOptions): Promise<GetOrderInfoResponse> {
-    Util.validateModel(request);
+  async getOrderInfoWithOptions(request: GetOrderInfoRequest, runtime: $dara.RuntimeOptions): Promise<GetOrderInfoResponse> {
+    request.validate();
     let query = { };
-    if (!Util.isUnset(request.listReleased)) {
+    if (!$dara.isNull(request.listReleased)) {
       query["ListReleased"] = request.listReleased;
     }
 
-    if (!Util.isUnset(request.relService)) {
+    if (!$dara.isNull(request.relService)) {
       query["RelService"] = request.relService;
     }
 
-    if (!Util.isUnset(request.resourceType)) {
+    if (!$dara.isNull(request.resourceType)) {
       query["ResourceType"] = request.resourceType;
     }
 
-    let req = new $OpenApi.OpenApiRequest({
+    let req = new $OpenApiUtil.OpenApiRequest({
       query: OpenApiUtil.query(query),
     });
-    let params = new $OpenApi.Params({
+    let params = new $OpenApiUtil.Params({
       action: "GetOrderInfo",
       version: "2021-07-30",
       protocol: "HTTPS",
@@ -349,46 +507,53 @@ export default class Client extends OpenApi {
       reqBodyType: "formData",
       bodyType: "json",
     });
-    return $tea.cast<GetOrderInfoResponse>(await this.callApi(params, req, runtime), new GetOrderInfoResponse({}));
+    if ($dara.isNull(this._signatureVersion) || this._signatureVersion != "v4") {
+      return $dara.cast<GetOrderInfoResponse>(await this.callApi(params, req, runtime), new GetOrderInfoResponse({}));
+    } else {
+      return $dara.cast<GetOrderInfoResponse>(await this.execute(params, req, runtime), new GetOrderInfoResponse({}));
+    }
+
   }
 
   /**
-   * @param request GetOrderInfoRequest
-   * @return GetOrderInfoResponse
+   * 获取在线license列表
+   * 
+   * @param request - GetOrderInfoRequest
+   * @returns GetOrderInfoResponse
    */
   async getOrderInfo(request: GetOrderInfoRequest): Promise<GetOrderInfoResponse> {
-    let runtime = new $Util.RuntimeOptions({ });
+    let runtime = new $dara.RuntimeOptions({ });
     return await this.getOrderInfoWithOptions(request, runtime);
   }
 
   /**
-   * @param request GetOrderUsageRequest
-   * @param runtime runtime options for this request RuntimeOptions
-   * @return GetOrderUsageResponse
+   * @param request - GetOrderUsageRequest
+   * @param runtime - runtime options for this request RuntimeOptions
+   * @returns GetOrderUsageResponse
    */
-  async getOrderUsageWithOptions(request: GetOrderUsageRequest, runtime: $Util.RuntimeOptions): Promise<GetOrderUsageResponse> {
-    Util.validateModel(request);
+  async getOrderUsageWithOptions(request: GetOrderUsageRequest, runtime: $dara.RuntimeOptions): Promise<GetOrderUsageResponse> {
+    request.validate();
     let query = { };
-    if (!Util.isUnset(request.licenseKey)) {
+    if (!$dara.isNull(request.licenseKey)) {
       query["LicenseKey"] = request.licenseKey;
     }
 
-    if (!Util.isUnset(request.relService)) {
+    if (!$dara.isNull(request.relService)) {
       query["RelService"] = request.relService;
     }
 
-    if (!Util.isUnset(request.resourceType)) {
+    if (!$dara.isNull(request.resourceType)) {
       query["ResourceType"] = request.resourceType;
     }
 
-    if (!Util.isUnset(request.timeRange)) {
+    if (!$dara.isNull(request.timeRange)) {
       query["TimeRange"] = request.timeRange;
     }
 
-    let req = new $OpenApi.OpenApiRequest({
+    let req = new $OpenApiUtil.OpenApiRequest({
       query: OpenApiUtil.query(query),
     });
-    let params = new $OpenApi.Params({
+    let params = new $OpenApiUtil.Params({
       action: "GetOrderUsage",
       version: "2021-07-30",
       protocol: "HTTPS",
@@ -399,15 +564,20 @@ export default class Client extends OpenApi {
       reqBodyType: "formData",
       bodyType: "json",
     });
-    return $tea.cast<GetOrderUsageResponse>(await this.callApi(params, req, runtime), new GetOrderUsageResponse({}));
+    if ($dara.isNull(this._signatureVersion) || this._signatureVersion != "v4") {
+      return $dara.cast<GetOrderUsageResponse>(await this.callApi(params, req, runtime), new GetOrderUsageResponse({}));
+    } else {
+      return $dara.cast<GetOrderUsageResponse>(await this.execute(params, req, runtime), new GetOrderUsageResponse({}));
+    }
+
   }
 
   /**
-   * @param request GetOrderUsageRequest
-   * @return GetOrderUsageResponse
+   * @param request - GetOrderUsageRequest
+   * @returns GetOrderUsageResponse
    */
   async getOrderUsage(request: GetOrderUsageRequest): Promise<GetOrderUsageResponse> {
-    let runtime = new $Util.RuntimeOptions({ });
+    let runtime = new $dara.RuntimeOptions({ });
     return await this.getOrderUsageWithOptions(request, runtime);
   }
 
