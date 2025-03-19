7ab01e27241da62afaf9a05247c9c2bf730ba754
sdk-team <sdk-team@alibabacloud.com>
2025-03-19 08:02:59 +0000

    Update API TextModerationPlus: update response param.

--- green-20220302/src/client.ts
+++ green-20220302/src/client.ts
@@ -3788,6 +3788,66 @@ export class TextModerationPlusResponseBodyDataAdvice extends $dara.Model {
   }
 }
 
+export class TextModerationPlusResponseBodyDataAttackResult extends $dara.Model {
+  /**
+   * @remarks
+   * The level of prompt attack
+   * 
+   * @example
+   * none
+   */
+  attackLevel?: string;
+  /**
+   * @remarks
+   * The confidence
+   * 
+   * @example
+   * 0
+   */
+  confidence?: number;
+  /**
+   * @remarks
+   * Description
+   * 
+   * @example
+   * safe
+   */
+  description?: string;
+  /**
+   * @remarks
+   * The label
+   * 
+   * @example
+   * safe
+   */
+  label?: string;
+  static names(): { [key: string]: string } {
+    return {
+      attackLevel: 'AttackLevel',
+      confidence: 'Confidence',
+      description: 'Description',
+      label: 'Label',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      attackLevel: 'string',
+      confidence: 'number',
+      description: 'string',
+      label: 'string',
+    };
+  }
+
+  validate() {
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class TextModerationPlusResponseBodyDataResultCustomizedHit extends $dara.Model {
   /**
    * @remarks
@@ -3898,12 +3958,92 @@ export class TextModerationPlusResponseBodyDataResult extends $dara.Model {
   }
 }
 
+export class TextModerationPlusResponseBodyDataSensitiveResult extends $dara.Model {
+  /**
+   * @remarks
+   * Description
+   * 
+   * @example
+   * xxx
+   */
+  description?: string;
+  /**
+   * @remarks
+   * The label
+   * 
+   * @example
+   * 1234
+   */
+  label?: string;
+  /**
+   * @remarks
+   * The sensitive data.
+   */
+  sensitiveData?: string[];
+  /**
+   * @remarks
+   * The level of sensitivity data
+   * 
+   * @example
+   * S1
+   */
+  sensitiveLevel?: string;
+  static names(): { [key: string]: string } {
+    return {
+      description: 'Description',
+      label: 'Label',
+      sensitiveData: 'SensitiveData',
+      sensitiveLevel: 'SensitiveLevel',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      description: 'string',
+      label: 'string',
+      sensitiveData: { 'type': 'array', 'itemType': 'string' },
+      sensitiveLevel: 'string',
+    };
+  }
+
+  validate() {
+    if(Array.isArray(this.sensitiveData)) {
+      $dara.Model.validateArray(this.sensitiveData);
+    }
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class TextModerationPlusResponseBodyData extends $dara.Model {
   /**
    * @remarks
    * The suggestion.
    */
   advice?: TextModerationPlusResponseBodyDataAdvice[];
+  /**
+   * @remarks
+   * The level of prompt attack
+   * 
+   * @example
+   * none
+   */
+  attackLevel?: string;
+  /**
+   * @remarks
+   * The result of prompt attack detect
+   */
+  attackResult?: TextModerationPlusResponseBodyDataAttackResult[];
+  /**
+   * @remarks
+   * The id of data
+   * 
+   * @example
+   * text1234
+   */
   dataId?: string;
   /**
    * @remarks
@@ -3926,23 +4066,44 @@ export class TextModerationPlusResponseBodyData extends $dara.Model {
    * 1
    */
   score?: number;
+  /**
+   * @remarks
+   * The level of sensitivity data
+   * 
+   * @example
+   * S0
+   */
+  sensitiveLevel?: string;
+  /**
+   * @remarks
+   * The result of sensitivity data detect
+   */
+  sensitiveResult?: TextModerationPlusResponseBodyDataSensitiveResult[];
   static names(): { [key: string]: string } {
     return {
       advice: 'Advice',
+      attackLevel: 'AttackLevel',
+      attackResult: 'AttackResult',
       dataId: 'DataId',
       result: 'Result',
       riskLevel: 'RiskLevel',
       score: 'Score',
+      sensitiveLevel: 'SensitiveLevel',
+      sensitiveResult: 'SensitiveResult',
     };
   }
 
   static types(): { [key: string]: any } {
     return {
       advice: { 'type': 'array', 'itemType': TextModerationPlusResponseBodyDataAdvice },
+      attackLevel: 'string',
+      attackResult: { 'type': 'array', 'itemType': TextModerationPlusResponseBodyDataAttackResult },
       dataId: 'string',
       result: { 'type': 'array', 'itemType': TextModerationPlusResponseBodyDataResult },
       riskLevel: 'string',
       score: 'number',
+      sensitiveLevel: 'string',
+      sensitiveResult: { 'type': 'array', 'itemType': TextModerationPlusResponseBodyDataSensitiveResult },
     };
   }
 
@@ -3950,8 +4111,14 @@ export class TextModerationPlusResponseBodyData extends $dara.Model {
     if(Array.isArray(this.advice)) {
       $dara.Model.validateArray(this.advice);
     }
+    if(Array.isArray(this.attackResult)) {
+      $dara.Model.validateArray(this.attackResult);
+    }
     if(Array.isArray(this.result)) {
       $dara.Model.validateArray(this.result);
+    }
+    if(Array.isArray(this.sensitiveResult)) {
+      $dara.Model.validateArray(this.sensitiveResult);
     }
     super.validate();
   }
