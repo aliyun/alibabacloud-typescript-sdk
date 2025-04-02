c80d9c79465dbf6f8f3f9c5392b7d647bbe3148d
sdk-team <sdk-team@alibabacloud.com>
2025-04-02 10:15:52 +0000

    Update API CreateWorkspaceDoc: add response parameters Body.dentryUuid.

--- aliding-20230426/src/client.ts
+++ aliding-20230426/src/client.ts
@@ -45044,6 +45044,11 @@ export class CreateWorkspaceDocShrinkRequest extends $dara.Model {
 export class CreateWorkspaceDocResponseBody extends $dara.Model {
   /**
    * @example
+   * YRBcvy
+   */
+  dentryUuid?: string;
+  /**
+   * @example
    * QoJGq7xxx
    */
   docKey?: string;
@@ -45067,25 +45072,41 @@ export class CreateWorkspaceDocResponseBody extends $dara.Model {
   url?: string;
   /**
    * @example
+   * 0FAAEC9C-C6C8-5C87-AF8E-1195889BBXXX
+   */
+  vendorRequestId?: string;
+  /**
+   * @example
+   * dingtalk
+   */
+  vendorType?: string;
+  /**
+   * @example
    * YRBGvy
    */
   workspaceId?: string;
   static names(): { [key: string]: string } {
     return {
+      dentryUuid: 'dentryUuid',
       docKey: 'docKey',
       nodeId: 'nodeId',
       requestId: 'requestId',
       url: 'url',
+      vendorRequestId: 'vendorRequestId',
+      vendorType: 'vendorType',
       workspaceId: 'workspaceId',
     };
   }
 
   static types(): { [key: string]: any } {
     return {
+      dentryUuid: 'string',
       docKey: 'string',
       nodeId: 'string',
       requestId: 'string',
       url: 'string',
+      vendorRequestId: 'string',
+      vendorType: 'string',
       workspaceId: 'string',
     };
   }
