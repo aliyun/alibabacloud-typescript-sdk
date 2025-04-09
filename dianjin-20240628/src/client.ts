879fb5c29bb661c43586ad87aeaa64b7011b7340
sdk-team <sdk-team@alibabacloud.com>
2025-04-09 08:13:24 +0000

    Support API GetDialogDetail.

--- dianjin-20240628/src/client.ts
+++ dianjin-20240628/src/client.ts
@@ -1234,6 +1234,142 @@ export class GetDialogAnalysisResultResponseBodyData extends $dara.Model {
   }
 }
 
+export class GetDialogDetailResponseBodyDataDialogueList extends $dara.Model {
+  content?: string;
+  /**
+   * @example
+   * 123761283
+   */
+  customerId?: string;
+  /**
+   * @example
+   * BOT
+   */
+  customerServiceId?: string;
+  /**
+   * @example
+   * 0
+   */
+  customerServiceType?: string;
+  /**
+   * @example
+   * true
+   */
+  hangUpDialog?: boolean;
+  /**
+   * @example
+   * 1742869659849
+   */
+  id?: number;
+  /**
+   * @example
+   * 193874634xxx
+   */
+  intentCode?: string;
+  intentName?: string;
+  /**
+   * @example
+   * 0
+   */
+  role?: string;
+  /**
+   * @example
+   * text
+   */
+  type?: string;
+  static names(): { [key: string]: string } {
+    return {
+      content: 'content',
+      customerId: 'customerId',
+      customerServiceId: 'customerServiceId',
+      customerServiceType: 'customerServiceType',
+      hangUpDialog: 'hangUpDialog',
+      id: 'id',
+      intentCode: 'intentCode',
+      intentName: 'intentName',
+      role: 'role',
+      type: 'type',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      content: 'string',
+      customerId: 'string',
+      customerServiceId: 'string',
+      customerServiceType: 'string',
+      hangUpDialog: 'boolean',
+      id: 'number',
+      intentCode: 'string',
+      intentName: 'string',
+      role: 'string',
+      type: 'string',
+    };
+  }
+
+  validate() {
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetDialogDetailResponseBodyData extends $dara.Model {
+  dialogueList?: GetDialogDetailResponseBodyDataDialogueList[];
+  /**
+   * @example
+   * 2024-09-27 11:23:20
+   */
+  gmtCreate?: string;
+  /**
+   * @example
+   * COMPLETED
+   */
+  status?: string;
+  /**
+   * @example
+   * 10
+   */
+  totalDialogTurns?: number;
+  /**
+   * @example
+   * 5
+   */
+  validDialogTurns?: number;
+  static names(): { [key: string]: string } {
+    return {
+      dialogueList: 'dialogueList',
+      gmtCreate: 'gmtCreate',
+      status: 'status',
+      totalDialogTurns: 'totalDialogTurns',
+      validDialogTurns: 'validDialogTurns',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      dialogueList: { 'type': 'array', 'itemType': GetDialogDetailResponseBodyDataDialogueList },
+      gmtCreate: 'string',
+      status: 'string',
+      totalDialogTurns: 'number',
+      validDialogTurns: 'number',
+    };
+  }
+
+  validate() {
+    if(Array.isArray(this.dialogueList)) {
+      $dara.Model.validateArray(this.dialogueList);
+    }
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class GetDocumentChunkListResponseBodyDataRecordsPos extends $dara.Model {
   axisArray?: number[];
   /**
@@ -8829,6 +8965,146 @@ export class GetDialogAnalysisResultResponse extends $dara.Model {
   }
 }
 
+export class GetDialogDetailRequest extends $dara.Model {
+  /**
+   * @remarks
+   * This parameter is required.
+   * 
+   * @example
+   * 1906623923815534xxx
+   */
+  sessionId?: string;
+  static names(): { [key: string]: string } {
+    return {
+      sessionId: 'sessionId',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      sessionId: 'string',
+    };
+  }
+
+  validate() {
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetDialogDetailResponseBody extends $dara.Model {
+  /**
+   * @example
+   * null
+   */
+  cost?: number;
+  data?: GetDialogDetailResponseBodyData;
+  /**
+   * @example
+   * null
+   */
+  dataType?: string;
+  /**
+   * @example
+   * 0
+   */
+  errCode?: string;
+  /**
+   * @example
+   * ok
+   */
+  message?: string;
+  /**
+   * @example
+   * 5E3FBAF1-17AF-53B7-AF0A-CDCEEB6DE658
+   */
+  requestId?: string;
+  /**
+   * @example
+   * true
+   */
+  success?: boolean;
+  /**
+   * @example
+   * 2024-04-24 11:54:34
+   */
+  time?: string;
+  static names(): { [key: string]: string } {
+    return {
+      cost: 'cost',
+      data: 'data',
+      dataType: 'dataType',
+      errCode: 'errCode',
+      message: 'message',
+      requestId: 'requestId',
+      success: 'success',
+      time: 'time',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      cost: 'number',
+      data: GetDialogDetailResponseBodyData,
+      dataType: 'string',
+      errCode: 'string',
+      message: 'string',
+      requestId: 'string',
+      success: 'boolean',
+      time: 'string',
+    };
+  }
+
+  validate() {
+    if(this.data && typeof (this.data as any).validate === 'function') {
+      (this.data as any).validate();
+    }
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetDialogDetailResponse extends $dara.Model {
+  headers?: { [key: string]: string };
+  statusCode?: number;
+  body?: GetDialogDetailResponseBody;
+  static names(): { [key: string]: string } {
+    return {
+      headers: 'headers',
+      statusCode: 'statusCode',
+      body: 'body',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      headers: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+      statusCode: 'number',
+      body: GetDialogDetailResponseBody,
+    };
+  }
+
+  validate() {
+    if(this.headers) {
+      $dara.Model.validateMap(this.headers);
+    }
+    if(this.body && typeof (this.body as any).validate === 'function') {
+      (this.body as any).validate();
+    }
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class GetDocumentChunkListRequest extends $dara.Model {
   chunkIdList?: string[];
   /**
@@ -14374,6 +14650,56 @@ export default class Client extends OpenApi {
     let runtime = new $dara.RuntimeOptions({ });
     let headers : {[key: string ]: string} = { };
     return await this.getDialogAnalysisResultWithOptions(workspaceId, request, headers, runtime);
+  }
+
+  /**
+   * 获取异步任务的结果
+   * 
+   * @param request - GetDialogDetailRequest
+   * @param headers - map
+   * @param runtime - runtime options for this request RuntimeOptions
+   * @returns GetDialogDetailResponse
+   */
+  async getDialogDetailWithOptions(workspaceId: string, request: GetDialogDetailRequest, headers: {[key: string ]: string}, runtime: $dara.RuntimeOptions): Promise<GetDialogDetailResponse> {
+    request.validate();
+    let query : {[key: string ]: any} = { };
+    if (!$dara.isNull(request.sessionId)) {
+      query["sessionId"] = request.sessionId;
+    }
+
+    let req = new $OpenApiUtil.OpenApiRequest({
+      headers: headers,
+      query: OpenApiUtil.query(query),
+    });
+    let params = new $OpenApiUtil.Params({
+      action: "GetDialogDetail",
+      version: "2024-06-28",
+      protocol: "HTTPS",
+      pathname: `/${$dara.URL.percentEncode(workspaceId)}/api/virtualHuman/dialog/detail`,
+      method: "GET",
+      authType: "AK",
+      style: "ROA",
+      reqBodyType: "json",
+      bodyType: "json",
+    });
+    if ($dara.isNull(this._signatureVersion) || this._signatureVersion != "v4") {
+      return $dara.cast<GetDialogDetailResponse>(await this.callApi(params, req, runtime), new GetDialogDetailResponse({}));
+    } else {
+      return $dara.cast<GetDialogDetailResponse>(await this.execute(params, req, runtime), new GetDialogDetailResponse({}));
+    }
+
+  }
+
+  /**
+   * 获取异步任务的结果
+   * 
+   * @param request - GetDialogDetailRequest
+   * @returns GetDialogDetailResponse
+   */
+  async getDialogDetail(workspaceId: string, request: GetDialogDetailRequest): Promise<GetDialogDetailResponse> {
+    let runtime = new $dara.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.getDialogDetailWithOptions(workspaceId, request, headers, runtime);
   }
 
   /**
