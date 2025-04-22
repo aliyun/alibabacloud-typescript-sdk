c17e4e21e6b06885b75b923254e169efedde181f
sdk-team <sdk-team@alibabacloud.com>
2025-04-22 03:15:18 +0000

    Generated ts 2025-04-21 for aps.

---
+++ aps-20250421/src/client.ts
@@ -0,0 +1,31 @@
+// This file is auto-generated, don't edit it
+/**
+ */
+import OpenApi from '@alicloud/openapi-core';
+import { OpenApiUtil, $OpenApiUtil } from '@alicloud/openapi-core';
+import * as $dara from '@darabonba/typescript';
+
+
+export default class Client extends OpenApi {
+
+  constructor(config: $OpenApiUtil.Config) {
+    super(config);
+    this._endpointRule = "";
+    this.checkConfig(config);
+    this._endpoint = this.getEndpoint("aps", this._regionId, this._endpointRule, this._network, this._suffix, this._endpointMap, this._endpoint);
+  }
+
+
+  getEndpoint(productId: string, regionId: string, endpointRule: string, network: string, suffix: string, endpointMap: {[key: string ]: string}, endpoint: string): string {
+    if (!$dara.isNull(endpoint)) {
+      return endpoint;
+    }
+
+    if (!$dara.isNull(endpointMap) && !$dara.isNull(endpointMap[regionId])) {
+      return endpointMap[regionId];
+    }
+
+    return OpenApiUtil.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+  }
+
+}
