e28c9780097840fd82c4ab5c70a89ba1ae2b1e13
sdk-team <sdk-team@alibabacloud.com>
2025-06-19 17:28:55 +0000

    Update API DiscoverEventSource: add response parameters Body.Data.SourceMySQLDiscovery.EstimatedRows.

--- eventbridge-20200401/src/models/DiscoverEventSourceResponseBodyDataSourceMySqldiscovery.ts
+++ eventbridge-20200401/src/models/DiscoverEventSourceResponseBodyDataSourceMySqldiscovery.ts
@@ -5,6 +5,8 @@ import { DiscoverEventSourceResponseBodyDataSourceMySQLDiscoveryTableSchema } fr
 
 export class DiscoverEventSourceResponseBodyDataSourceMySQLDiscovery extends $dara.Model {
   databaseNames?: string[];
+  estimatedRows?: number;
+  expireLogsDays?: number;
   /**
    * @example
    * [{\\"is_active\\":\\"1\\",\\"name\\":\\"0c0c5d1a-e844-44a8-902d-4f62cbcb0479\\",\\"id\\":\\"21\\"},{\\"is_active\\":\\"1\\",\\"name\\":\\"18ef033e-70bb-4795-8222-6cfb54f38f26\\",\\"id\\":\\"22\\"}]
@@ -12,21 +14,28 @@ export class DiscoverEventSourceResponseBodyDataSourceMySQLDiscovery extends $da
   simpleData?: string;
   tableNames?: string[];
   tableSchema?: DiscoverEventSourceResponseBodyDataSourceMySQLDiscoveryTableSchema;
+  waitTimeout?: number;
   static names(): { [key: string]: string } {
     return {
       databaseNames: 'DatabaseNames',
+      estimatedRows: 'EstimatedRows',
+      expireLogsDays: 'ExpireLogsDays',
       simpleData: 'SimpleData',
       tableNames: 'TableNames',
       tableSchema: 'TableSchema',
+      waitTimeout: 'WaitTimeout',
     };
   }
 
   static types(): { [key: string]: any } {
     return {
       databaseNames: { 'type': 'array', 'itemType': 'string' },
+      estimatedRows: 'number',
+      expireLogsDays: 'number',
       simpleData: 'string',
       tableNames: { 'type': 'array', 'itemType': 'string' },
       tableSchema: DiscoverEventSourceResponseBodyDataSourceMySQLDiscoveryTableSchema,
+      waitTimeout: 'number',
     };
   }
 
