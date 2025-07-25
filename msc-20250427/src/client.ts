3c629d097490e13bc4dbf7770f9a0db1c3b68c80
sdk-team <sdk-team@alibabacloud.com>
2025-07-25 07:23:52 +0000

    Generated ts 2025-04-27 for Msc.

---
+++ msc-20250427/src/client.ts
@@ -0,0 +1,32 @@
+// This file is auto-generated, don't edit it
+import * as $dara from '@darabonba/typescript';
+import OpenApi from '@alicloud/openapi-core';
+import { OpenApiUtil, $OpenApiUtil }from '@alicloud/openapi-core';
+
+
+/**
+ */
+
+export default class Client extends OpenApi {
+
+  constructor(config: $OpenApiUtil.Config) {
+    super(config);
+    this._endpointRule = "";
+    this.checkConfig(config);
+    this._endpoint = this.getEndpoint("msc", this._regionId, this._endpointRule, this._network, this._suffix, this._endpointMap, this._endpoint);
+  }
+
+
+  getEndpoint(productId: string, regionId: string, endpointRule: string, network: string, suffix: string, endpointMap: {[key: string ]: string}, endpoint: string): string {
+    if (!$dara.isNull(endpoint)) {
+      return endpoint;
+    }
+
+    if (!$dara.isNull(endpointMap) && !$dara.isNull(endpointMap[regionId])) {
+      return endpointMap[regionId];
+    }
+
+    return OpenApiUtil.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+  }
+
+}
