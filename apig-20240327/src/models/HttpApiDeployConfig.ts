b09d653c69f6008fab01812bd124721ef3e5966e
sdk-team <sdk-team@alibabacloud.com>
2026-01-08 06:22:13 +0000

    Generated ts 2024-03-27 for APIG.

--- apig-20240327/src/models/HttpApiDeployConfig.ts
+++ apig-20240327/src/models/HttpApiDeployConfig.ts
@@ -86,8 +86,358 @@ export class HttpApiDeployConfigPolicyConfigsAiFallbackConfig extends $dara.Mode
   }
 }
 
+export class HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfigConsumerRequestCheckService extends $dara.Model {
+  matchType?: string;
+  modalityType?: string;
+  name?: string;
+  requestCheckService?: string;
+  requestImageCheckService?: string;
+  static names(): { [key: string]: string } {
+    return {
+      matchType: 'matchType',
+      modalityType: 'modalityType',
+      name: 'name',
+      requestCheckService: 'requestCheckService',
+      requestImageCheckService: 'requestImageCheckService',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      matchType: 'string',
+      modalityType: 'string',
+      name: 'string',
+      requestCheckService: 'string',
+      requestImageCheckService: 'string',
+    };
+  }
+
+  validate() {
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfigConsumerResponseCheckService extends $dara.Model {
+  matchType?: string;
+  modalityType?: string;
+  name?: string;
+  responseCheckService?: string;
+  responseImageCheckService?: string;
+  static names(): { [key: string]: string } {
+    return {
+      matchType: 'matchType',
+      modalityType: 'modalityType',
+      name: 'name',
+      responseCheckService: 'responseCheckService',
+      responseImageCheckService: 'responseImageCheckService',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      matchType: 'string',
+      modalityType: 'string',
+      name: 'string',
+      responseCheckService: 'string',
+      responseImageCheckService: 'string',
+    };
+  }
+
+  validate() {
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfigConsumerRiskLevel extends $dara.Model {
+  level?: string;
+  matchType?: string;
+  name?: string;
+  type?: string;
+  static names(): { [key: string]: string } {
+    return {
+      level: 'level',
+      matchType: 'matchType',
+      name: 'name',
+      type: 'type',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      level: 'string',
+      matchType: 'string',
+      name: 'string',
+      type: 'string',
+    };
+  }
+
+  validate() {
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfigRiskConfigConsumerRules extends $dara.Model {
+  matchType?: string;
+  pattern?: string;
+  static names(): { [key: string]: string } {
+    return {
+      matchType: 'matchType',
+      pattern: 'pattern',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      matchType: 'string',
+      pattern: 'string',
+    };
+  }
+
+  validate() {
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfigRiskConfig extends $dara.Model {
+  consumerRules?: HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfigRiskConfigConsumerRules;
+  level?: string;
+  type?: string;
+  static names(): { [key: string]: string } {
+    return {
+      consumerRules: 'consumerRules',
+      level: 'level',
+      type: 'type',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      consumerRules: HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfigRiskConfigConsumerRules,
+      level: 'string',
+      type: 'string',
+    };
+  }
+
+  validate() {
+    if(this.consumerRules && typeof (this.consumerRules as any).validate === 'function') {
+      (this.consumerRules as any).validate();
+    }
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfig extends $dara.Model {
+  bufferLimit?: number;
+  checkRequest?: boolean;
+  checkRequestImage?: boolean;
+  checkResponse?: boolean;
+  checkResponseImage?: boolean;
+  consumerRequestCheckService?: HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfigConsumerRequestCheckService[];
+  consumerResponseCheckService?: HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfigConsumerResponseCheckService[];
+  consumerRiskLevel?: HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfigConsumerRiskLevel[];
+  requestCheckService?: string;
+  requestImageCheckService?: string;
+  responseCheckService?: string;
+  responseImageCheckService?: string;
+  riskAlertLevel?: string;
+  riskConfig?: HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfigRiskConfig[];
+  serviceAddress?: string;
+  static names(): { [key: string]: string } {
+    return {
+      bufferLimit: 'bufferLimit',
+      checkRequest: 'checkRequest',
+      checkRequestImage: 'checkRequestImage',
+      checkResponse: 'checkResponse',
+      checkResponseImage: 'checkResponseImage',
+      consumerRequestCheckService: 'consumerRequestCheckService',
+      consumerResponseCheckService: 'consumerResponseCheckService',
+      consumerRiskLevel: 'consumerRiskLevel',
+      requestCheckService: 'requestCheckService',
+      requestImageCheckService: 'requestImageCheckService',
+      responseCheckService: 'responseCheckService',
+      responseImageCheckService: 'responseImageCheckService',
+      riskAlertLevel: 'riskAlertLevel',
+      riskConfig: 'riskConfig',
+      serviceAddress: 'serviceAddress',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      bufferLimit: 'number',
+      checkRequest: 'boolean',
+      checkRequestImage: 'boolean',
+      checkResponse: 'boolean',
+      checkResponseImage: 'boolean',
+      consumerRequestCheckService: { 'type': 'array', 'itemType': HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfigConsumerRequestCheckService },
+      consumerResponseCheckService: { 'type': 'array', 'itemType': HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfigConsumerResponseCheckService },
+      consumerRiskLevel: { 'type': 'array', 'itemType': HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfigConsumerRiskLevel },
+      requestCheckService: 'string',
+      requestImageCheckService: 'string',
+      responseCheckService: 'string',
+      responseImageCheckService: 'string',
+      riskAlertLevel: 'string',
+      riskConfig: { 'type': 'array', 'itemType': HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfigRiskConfig },
+      serviceAddress: 'string',
+    };
+  }
+
+  validate() {
+    if(Array.isArray(this.consumerRequestCheckService)) {
+      $dara.Model.validateArray(this.consumerRequestCheckService);
+    }
+    if(Array.isArray(this.consumerResponseCheckService)) {
+      $dara.Model.validateArray(this.consumerResponseCheckService);
+    }
+    if(Array.isArray(this.consumerRiskLevel)) {
+      $dara.Model.validateArray(this.consumerRiskLevel);
+    }
+    if(Array.isArray(this.riskConfig)) {
+      $dara.Model.validateArray(this.riskConfig);
+    }
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class HttpApiDeployConfigPolicyConfigsAiTokenRateLimitConfigGlobalRules extends $dara.Model {
+  limitMode?: string;
+  limitType?: string;
+  limitValue?: number;
+  matchKey?: string;
+  matchType?: string;
+  matchValue?: string;
+  static names(): { [key: string]: string } {
+    return {
+      limitMode: 'limitMode',
+      limitType: 'limitType',
+      limitValue: 'limitValue',
+      matchKey: 'matchKey',
+      matchType: 'matchType',
+      matchValue: 'matchValue',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      limitMode: 'string',
+      limitType: 'string',
+      limitValue: 'number',
+      matchKey: 'string',
+      matchType: 'string',
+      matchValue: 'string',
+    };
+  }
+
+  validate() {
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class HttpApiDeployConfigPolicyConfigsAiTokenRateLimitConfigRules extends $dara.Model {
+  limitMode?: string;
+  limitType?: string;
+  limitValue?: number;
+  matchKey?: string;
+  matchType?: string;
+  matchValue?: string;
+  static names(): { [key: string]: string } {
+    return {
+      limitMode: 'limitMode',
+      limitType: 'limitType',
+      limitValue: 'limitValue',
+      matchKey: 'matchKey',
+      matchType: 'matchType',
+      matchValue: 'matchValue',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      limitMode: 'string',
+      limitType: 'string',
+      limitValue: 'number',
+      matchKey: 'string',
+      matchType: 'string',
+      matchValue: 'string',
+    };
+  }
+
+  validate() {
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class HttpApiDeployConfigPolicyConfigsAiTokenRateLimitConfig extends $dara.Model {
+  enableGlobalRules?: boolean;
+  globalRules?: HttpApiDeployConfigPolicyConfigsAiTokenRateLimitConfigGlobalRules[];
+  rules?: HttpApiDeployConfigPolicyConfigsAiTokenRateLimitConfigRules[];
+  static names(): { [key: string]: string } {
+    return {
+      enableGlobalRules: 'enableGlobalRules',
+      globalRules: 'globalRules',
+      rules: 'rules',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      enableGlobalRules: 'boolean',
+      globalRules: { 'type': 'array', 'itemType': HttpApiDeployConfigPolicyConfigsAiTokenRateLimitConfigGlobalRules },
+      rules: { 'type': 'array', 'itemType': HttpApiDeployConfigPolicyConfigsAiTokenRateLimitConfigRules },
+    };
+  }
+
+  validate() {
+    if(Array.isArray(this.globalRules)) {
+      $dara.Model.validateArray(this.globalRules);
+    }
+    if(Array.isArray(this.rules)) {
+      $dara.Model.validateArray(this.rules);
+    }
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class HttpApiDeployConfigPolicyConfigs extends $dara.Model {
   aiFallbackConfig?: HttpApiDeployConfigPolicyConfigsAiFallbackConfig;
+  aiSecurityGuardConfig?: HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfig;
+  aiTokenRateLimitConfig?: HttpApiDeployConfigPolicyConfigsAiTokenRateLimitConfig;
   /**
    * @example
    * true
@@ -101,6 +451,8 @@ export class HttpApiDeployConfigPolicyConfigs extends $dara.Model {
   static names(): { [key: string]: string } {
     return {
       aiFallbackConfig: 'aiFallbackConfig',
+      aiSecurityGuardConfig: 'aiSecurityGuardConfig',
+      aiTokenRateLimitConfig: 'aiTokenRateLimitConfig',
       enable: 'enable',
       type: 'type',
     };
@@ -109,6 +461,8 @@ export class HttpApiDeployConfigPolicyConfigs extends $dara.Model {
   static types(): { [key: string]: any } {
     return {
       aiFallbackConfig: HttpApiDeployConfigPolicyConfigsAiFallbackConfig,
+      aiSecurityGuardConfig: HttpApiDeployConfigPolicyConfigsAiSecurityGuardConfig,
+      aiTokenRateLimitConfig: HttpApiDeployConfigPolicyConfigsAiTokenRateLimitConfig,
       enable: 'boolean',
       type: 'string',
     };
@@ -117,6 +471,12 @@ export class HttpApiDeployConfigPolicyConfigs extends $dara.Model {
   validate() {
     if(this.aiFallbackConfig && typeof (this.aiFallbackConfig as any).validate === 'function') {
       (this.aiFallbackConfig as any).validate();
+    }
+    if(this.aiSecurityGuardConfig && typeof (this.aiSecurityGuardConfig as any).validate === 'function') {
+      (this.aiSecurityGuardConfig as any).validate();
+    }
+    if(this.aiTokenRateLimitConfig && typeof (this.aiTokenRateLimitConfig as any).validate === 'function') {
+      (this.aiTokenRateLimitConfig as any).validate();
     }
     super.validate();
   }
