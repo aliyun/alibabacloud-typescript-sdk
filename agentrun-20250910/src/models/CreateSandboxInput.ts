6dae3a3cd1c5e21561810708c77968aa9ecb02e7
sdk-team <sdk-team@alibabacloud.com>
2026-03-11 08:03:24 +0000

    Generated ts 2025-09-10 for AgentRun.

--- agentrun-20250910/src/models/CreateSandboxInput.ts
+++ agentrun-20250910/src/models/CreateSandboxInput.ts
@@ -2,11 +2,13 @@
 import * as $dara from '@darabonba/typescript';
 import { NASConfig } from "./Nasconfig";
 import { OSSMountConfig } from "./OssmountConfig";
+import { PolarFsConfig } from "./PolarFsConfig";
 
 
 export class CreateSandboxInput extends $dara.Model {
   nasConfig?: NASConfig;
   ossMountConfig?: OSSMountConfig;
+  polarFsConfig?: PolarFsConfig;
   /**
    * @example
    * 01KE8DAJ35JC8SKP9CNFRZ8CW7
@@ -37,6 +39,7 @@ export class CreateSandboxInput extends $dara.Model {
     return {
       nasConfig: 'nasConfig',
       ossMountConfig: 'ossMountConfig',
+      polarFsConfig: 'polarFsConfig',
       sandboxId: 'sandboxId',
       sandboxIdleTimeoutInSeconds: 'sandboxIdleTimeoutInSeconds',
       sandboxIdleTimeoutSeconds: 'sandboxIdleTimeoutSeconds',
@@ -48,6 +51,7 @@ export class CreateSandboxInput extends $dara.Model {
     return {
       nasConfig: NASConfig,
       ossMountConfig: OSSMountConfig,
+      polarFsConfig: PolarFsConfig,
       sandboxId: 'string',
       sandboxIdleTimeoutInSeconds: 'number',
       sandboxIdleTimeoutSeconds: 'number',
@@ -61,6 +65,9 @@ export class CreateSandboxInput extends $dara.Model {
     }
     if(this.ossMountConfig && typeof (this.ossMountConfig as any).validate === 'function') {
       (this.ossMountConfig as any).validate();
+    }
+    if(this.polarFsConfig && typeof (this.polarFsConfig as any).validate === 'function') {
+      (this.polarFsConfig as any).validate();
     }
     super.validate();
   }
