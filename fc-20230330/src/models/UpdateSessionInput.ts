a494b3e3f0fdc3e63ceedc892e0e43e4ef9dd055
sdk-team <sdk-team@alibabacloud.com>
2026-03-21 17:16:50 +0000

    Generated ts 2023-03-30 for FC.

--- fc-20230330/src/models/UpdateSessionInput.ts
+++ fc-20230330/src/models/UpdateSessionInput.ts
@@ -1,9 +1,15 @@
 // This file is auto-generated, don't edit it
 import * as $dara from '@darabonba/typescript';
+import { NASConfig } from "./Nasconfig";
+import { OSSMountConfig } from "./OssmountConfig";
+import { PolarFsConfig } from "./PolarFsConfig";
 
 
 export class UpdateSessionInput extends $dara.Model {
   disableSessionIdReuse?: boolean;
+  nasConfig?: NASConfig;
+  ossMountConfig?: OSSMountConfig;
+  polarFsConfig?: PolarFsConfig;
   /**
    * @example
    * 1800
@@ -17,6 +23,9 @@ export class UpdateSessionInput extends $dara.Model {
   static names(): { [key: string]: string } {
     return {
       disableSessionIdReuse: 'disableSessionIdReuse',
+      nasConfig: 'nasConfig',
+      ossMountConfig: 'ossMountConfig',
+      polarFsConfig: 'polarFsConfig',
       sessionIdleTimeoutInSeconds: 'sessionIdleTimeoutInSeconds',
       sessionTTLInSeconds: 'sessionTTLInSeconds',
     };
@@ -25,12 +34,24 @@ export class UpdateSessionInput extends $dara.Model {
   static types(): { [key: string]: any } {
     return {
       disableSessionIdReuse: 'boolean',
+      nasConfig: NASConfig,
+      ossMountConfig: OSSMountConfig,
+      polarFsConfig: PolarFsConfig,
       sessionIdleTimeoutInSeconds: 'number',
       sessionTTLInSeconds: 'number',
     };
   }
 
   validate() {
+    if(this.nasConfig && typeof (this.nasConfig as any).validate === 'function') {
+      (this.nasConfig as any).validate();
+    }
+    if(this.ossMountConfig && typeof (this.ossMountConfig as any).validate === 'function') {
+      (this.ossMountConfig as any).validate();
+    }
+    if(this.polarFsConfig && typeof (this.polarFsConfig as any).validate === 'function') {
+      (this.polarFsConfig as any).validate();
+    }
     super.validate();
   }
 
