d384be25af2e8cbdd9150369009139e61e201787
sdk-team <sdk-team@alibabacloud.com>
2026-04-15 15:24:44 +0000

    Generated ts 2026-04-14 for DocumentParseService.

---
+++ documentparseservice-20260414/src/models/model.ts
@@ -0,0 +1,5 @@
+export { DocumentParseTestApiResponseBodyDataUsage } from './DocumentParseTestApiResponseBody';
+export { DocumentParseTestApiResponseBodyData } from './DocumentParseTestApiResponseBody';
+export { DocumentParseTestApiRequest } from './DocumentParseTestApiRequest';
+export { DocumentParseTestApiResponseBody } from './DocumentParseTestApiResponseBody';
+export { DocumentParseTestApiResponse } from './DocumentParseTestApiResponse';
