f5f12a2f3cef13aa3f7262c08f7b7c3d0797a5c4
sdk-team <sdk-team@alibabacloud.com>
2026-08-05 04:15:47 +0000

    Generated ts 2022-01-12 for PaiStudio.

--- paistudio-20220112/src/client.ts
+++ paistudio-20220112/src/client.ts
@@ -1687,7 +1687,7 @@ export default class Client extends OpenApi {
    * @remarks
    * ## Operation description
    * - This feature is currently available only on the Lingjun AI Computing Service platform and is accessible to customers through a whitelist.
-   * - `NodeName` and `ResourceGroupId` are required parameters that represent the node name and the ID of the resource group to which the node belongs.
+   * - `NodeName` and `ResourceGroupId` are required parameters that represent the node name and the resource group ID to which the node belongs.
    * - Optional parameters include `OversoldTypes` and `GPUIndexes`, which are used to further filter or specify conditions.
    * 
    * @param request - ListNodePodsRequest
@@ -1734,7 +1734,7 @@ export default class Client extends OpenApi {
    * @remarks
    * ## Operation description
    * - This feature is currently available only on the Lingjun AI Computing Service platform and is accessible to customers through a whitelist.
-   * - `NodeName` and `ResourceGroupId` are required parameters that represent the node name and the ID of the resource group to which the node belongs.
+   * - `NodeName` and `ResourceGroupId` are required parameters that represent the node name and the resource group ID to which the node belongs.
    * - Optional parameters include `OversoldTypes` and `GPUIndexes`, which are used to further filter or specify conditions.
    * 
    * @param request - ListNodePodsRequest
--- paistudio-20220112/src/models/NodePodInfo.ts
+++ paistudio-20220112/src/models/NodePodInfo.ts
@@ -5,38 +5,66 @@ import { ResourceAmount } from "./ResourceAmount";
 
 export class NodePodInfo extends $dara.Model {
   /**
+   * @remarks
+   * The occupied GPU indexes.
+   */
+  GPUIndexes?: number[];
+  /**
+   * @remarks
+   * The Pod status.
+   * 
    * @example
    * Running
    */
   phase?: string;
   /**
+   * @remarks
+   * The IP address of the Pod.
+   * 
    * @example
    * 192.168.2.2
    */
   podIP?: string;
   /**
+   * @remarks
+   * The Pod name.
+   * 
    * @example
    * test
    */
   podName?: string;
   /**
+   * @remarks
+   * The namespace of the Pod.
+   * 
    * @example
    * test
    */
   podNamespace?: string;
+  /**
+   * @remarks
+   * The resource usage information.
+   */
   resourceSpec?: ResourceAmount;
   /**
+   * @remarks
+   * The task ID or service ID.
+   * 
    * @example
    * dlc19de9s6vn3acr
    */
   workloadId?: string;
   /**
+   * @remarks
+   * The sub-product that the Pod belongs to.
+   * 
    * @example
    * dlc
    */
   workloadType?: string;
   static names(): { [key: string]: string } {
     return {
+      GPUIndexes: 'GPUIndexes',
       phase: 'Phase',
       podIP: 'PodIP',
       podName: 'PodName',
@@ -49,6 +77,7 @@ export class NodePodInfo extends $dara.Model {
 
   static types(): { [key: string]: any } {
     return {
+      GPUIndexes: { 'type': 'array', 'itemType': 'number' },
       phase: 'string',
       podIP: 'string',
       podName: 'string',
@@ -60,6 +89,9 @@ export class NodePodInfo extends $dara.Model {
   }
 
   validate() {
+    if(Array.isArray(this.GPUIndexes)) {
+      $dara.Model.validateArray(this.GPUIndexes);
+    }
     if(this.resourceSpec && typeof (this.resourceSpec as any).validate === 'function') {
       (this.resourceSpec as any).validate();
     }
